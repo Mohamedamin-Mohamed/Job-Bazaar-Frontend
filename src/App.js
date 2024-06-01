6f892ec1fb055a752104f8bab2995786dc0fa7a0
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-06-01 11:57:25 -0500

    added routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import Home from "./Home";
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Routes} from "react-router-dom";
+import HomeTile from "./HomeTile";
+import Login from "./Login";
+import Signup from "./Signup";
+import EmailLookup from "./EmailLookup";
+import PasswordReset from "./PasswordReset";
 
-function App() {
-  return (
-      <div>
-          <Home />
-      </div>
-
-  );
+const router = createBrowserRouter(
+        createRoutesFromElements(
+            <Route>
+                <Route path="/" element={<Home />} />
+                <Route path="/accounts" element={<HomeTile />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route index path="/login/email-lookup" element={<EmailLookup />} />
+                <Route path="/login/password-reset" element={<PasswordReset />} />
+                    <Route path="/signup" element={<Signup />} />
+            </Route>
+        )
+)
+const App = ()=>{
+    return(
+        <RouterProvider router={router} />
+    )
 }
-
-export default App;
+export default App
