c39efbf136a2a8344219239466f8ae40defe09d1
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-06-01 11:58:38 -0500

    to be accessed at /accounts

--- src/HomeTile.js
+++ src/HomeTile.js
@@ -2,12 +2,15 @@ import Image from './images.jfif'
 import React, {useState} from "react";
 import { useDispatch, useSelector} from "react-redux";
 import {setHomeTileShow, setLoginShow} from "./Redux/UserSlice";
+import {NavLink, useNavigate} from "react-router-dom";
 
 const HomeTile = ()=>{
     const dispatch = useDispatch()
+    const navigate = useNavigate()
     const handleLogin = ()=>{
-        dispatch(setHomeTileShow(false))
-        dispatch(setLoginShow(true))
+        // dispatch(setHomeTileShow(false))
+        // dispatch(setLoginShow(true))
+        navigate("/login")
     }
 
    const usr = useSelector(state => state.userInfo)
@@ -15,14 +18,14 @@ const HomeTile = ()=>{
         <div className= 'flex justify-center items-center h-screen'>
             <div className= 'flex h-[250px] justify-center items-center mx-6'>
            <div className= 'border rounded-lg'>
-               <img src={Image} className= 'h-[230px]'/>
+               <img alt="" src={Image} className= 'h-[230px]'/>
            </div>
             <div className= 'border rounded-lg'>
                 <div className= 'm-4'>
                 <h1 className= 'text-xl font-bold m-3 text-center'>Welcome back</h1>
                 <p className= 'text-xl'>To keep connected with us please login with your personal info</p>
                     <div className= 'flex justify-center'>
-                <button className= 'bg-blue-600 w-[50%] rounded-xl p-3 my-8' onClick={ handleLogin }>Sign In</button>
+                <button className= 'bg-blue-600 w-[50%] rounded-xl p-3 my-8' onClick={handleLogin}>Sign In</button>
                     </div>
                 </div>
             </div>
