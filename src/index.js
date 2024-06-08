ac8565a815bb4fecfa53c2033856b548de170823
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-06-07 23:52:39 -0500

    fix: removed dotenv package

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import { Provider } from 'react-redux'
 import Store from './Redux/Store'
 import {GoogleOAuthProvider} from "@react-oauth/google";
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
 root.render(
-    <GoogleOAuthProvider clientId={"488140599212-l1llbad4vqlg4ogdcpu7qktshu6u6qov.apps.googleusercontent.com"} >
     <Provider store={Store} >
     <App />
     </Provider>
-    </GoogleOAuthProvider>
 );
