cb95f0202e783c123a58e558b348820e7fed5d48
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-06-07 23:10:35 -0500

    fix: renamed the routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
-import Home from "./Home";
-import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Routes} from "react-router-dom";
+
+import {createBrowserRouter, createRoutesFromElements, Outlet, Route, RouterProvider, Routes} from "react-router-dom";
 import HomeTile from "./HomeTile";
 import Login from "./Login";
 import Signup from "./Signup";
 import EmailLookup from "./EmailLookup";
 import PasswordReset from "./PasswordReset";
+import Home from "./Home";
 
 const router = createBrowserRouter(
         createRoutesFromElements(
+            //
+            //     <Route path="/" element={<Home />}>
+            //     <Route path="/accounts" element={<HomeTile />}/>
+            //         <Route path="/accounts/login" element={<Login />} />
+            //     <Route path="/accounts/signup" element={<Signup />} />
+            //         <Route index path="/login/email-lookup" element={<EmailLookup />} />
+            //     <Route path="/login/password-reset" element={<PasswordReset />} />
+            // </Route>
             <Route>
                 <Route path="/" element={<Home />} />
-                <Route path="/accounts" element={<HomeTile />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route index path="/login/email-lookup" element={<EmailLookup />} />
-                <Route path="/login/password-reset" element={<PasswordReset />} />
-                    <Route path="/signup" element={<Signup />} />
+                <Route path="/accounts/login" element={<Login />} />
+                <Route index path="accounts/login/email-lookup" element={<EmailLookup />} />
+                <Route path="accounts/login/password-reset" element={<PasswordReset />} />
+                <Route path="accounts/signup" element={<Signup />} />
             </Route>
 
         )
