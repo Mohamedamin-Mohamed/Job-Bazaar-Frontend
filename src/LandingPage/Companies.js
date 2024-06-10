4db316e50dfb2e1f7b7e3038837198102a659856
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-06-09 19:45:49 -0500

    feat: component to show the company feature

---
+++ src/LandingPage/Companies.js
@@ -0,0 +1,17 @@
+import CommunityImage from '../Images/locked-companies.webp'
+const Companies = ({setCompanies})=>{
+    return(
+        <div className="fixed inset-0 flex items-start justify-start z-50 mt-24 bg-inherit bg-black ml-80" onMouseLeave={()=> setCompanies(false)}>
+            <div className="border-l border-b w-[28%] h-[245px] pl-4 pt-4">
+                <p className="font-medium">Read millions of reviews</p>
+                <p className="w-[80%]">Read anonymous reviews on over 600,000 companies worldwide from the people that work there.</p>
+                <button className="border border-gray-500 mt-8  ml-10 p-2.5 rounded-md hover:bg-blue-600">Start using Job Bazaar</button>
+            </div>
+            <div className="border-r border-b h-[245px]">
+                <img src={CommunityImage} alt="" className="mr-4"/>
+            </div>
+
+        </div>
+    )
+}
+export default Companies
