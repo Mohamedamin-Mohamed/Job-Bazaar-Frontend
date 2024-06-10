a1a4ea6edf9a129710a86da4d7bf1d83a4b9b041
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-06-10 16:26:20 -0500

    feat: added toggle between LoginPanel and PanelShowCase components

--- src/LandingPage/AuthShowcase.js
+++ src/LandingPage/AuthShowcase.js
@@ -3,16 +3,22 @@ import {FcGoogle} from "react-icons/fc";
 import {FaGithub} from "react-icons/fa";
 import ImageRight from "../Images/c.svg";
 import LoginPanel from "./LoginPanel";
+import {useDispatch, useSelector} from "react-redux";
+import PanelShowcase from "./PanelShowcase";
 
 const AuthShowcase = ()=>{
+    const usr = useSelector(state => state.userInfo)
+    const dispatch = useDispatch()
+
     return(
         <>
-            <h1 className="text-center md:text-5xl text-4xl text-[#00A264] font-medium py-6">Your work people are here</h1>
-            <div className="flex lg:flex-row flex-col mt-24 items-center xl:justify-evenly ">
+            <h1 className="text-center md:text-5xl text-4xl text-[#00A264] font-medium pt-10">Your work people are here</h1>
+            <div className="flex lg:flex-row flex-col items-center xl:justify-evenly ">
                 <div className="lg:ml-28">
                     <img src={ImageLeft} alt="" className="h-[450px] w-[500px]"/>
                 </div>
-                <LoginPanel />
+                {usr.firstPanel && <LoginPanel />}
+                {usr.secondPanel && <PanelShowcase />}
                 <div className="lg:ml-24 ml-24">
                     <img src={ImageRight} alt="" className="h-[500px] w-[500px]"/>
                 </div>
