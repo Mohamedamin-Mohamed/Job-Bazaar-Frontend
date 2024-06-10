4f73b787d26b449bbf3691147cf4b602c429ebd9
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-06-10 12:01:03 -0500

    fix: serving AuthShowCase instead of LoginPanel component

--- src/LandingPage/Hero.js
+++ src/LandingPage/Hero.js
@@ -2,19 +2,18 @@ import {RiCommunityFill} from "react-icons/ri";
 import {MdOutlineAssuredWorkload} from "react-icons/md";
 import {TbClipboardSmile} from "react-icons/tb";
 import {HiBanknotes} from "react-icons/hi2";
-import LoginPanel from "./LoginPanel";
+import AuthShowcase from "./AuthShowcase";
 
 const Hero = ()=>{
     return(
         <div>
-
-            <LoginPanel />
+            <AuthShowcase />
             <div className="flex flex-col text-center mt-4">
                 <h2 className="text-3xl text-[#20262e] border-t pt-5">Get ahead with Job Bazaar</h2>
                 <p className="font-medium text-xl my-4">We're serving up trusted insights and anonymous conversation, so
                     you'll have the goods you need to succeed</p>
             </div>
-            <div className="flex lg:flex-row flex-col justify-center space-x-12 my-6 text-lg">
+            <div className="flex lg:flex-row flex-col justify-center space-x-12 mb-6 text-lg">
                 <div className="flex justify-center space-x-12 my-4">
                     <div className="flex-col ml-8">
                         <RiCommunityFill className="ml-24" size={50} color="green"/>
