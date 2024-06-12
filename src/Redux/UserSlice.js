4cd58e4f04ec95ab5f1c9c676d588679c066751b
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-06-11 21:51:01 -0500

    chore: renamed user email variable

--- src/Redux/UserSlice.js
+++ src/Redux/UserSlice.js
@@ -5,29 +5,37 @@ const initialState = {
     firstPanel: true,
     secondPanel: false,
     credentials: true,
-    usrEmail: ''
+    usrEmail: null,
+    firstPanel2: false,
+    secondPanel2: false
     }
     const UserSlice = createSlice({
         name: 'userInfo',
         initialState: initialState,
         reducers: {
-        setLoading : (state, action)=>({
+            setLoading : (state, action)=>({
             ...state, loading: action.payload
         }),
-        setFirstPanel: (state, action) =>({
+            setFirstPanel: (state, action) =>({
             ...state, firstPanel: action.payload
             }),
-        setSecondPanel: (state, action) =>({
+            setSecondPanel: (state, action) =>({
             ...state, secondPanel: action.payload
         }),
-        setCredentials: (state, action) =>({
+            setCredentials: (state, action) =>({
             ...state, credentials: action.payload
         }),
             setUsrEmail: (state, action) =>({
                 ...state, usrEmail: action.payload
+            }),
+            setFirstPanel2 : (state, action) =>({
+                ...state, firstPanel2: action.payload
+            }),
+            setSecondPanel2: (state, action) =>({
+                ...state, secondPanel2: action.payload
             })
     }
 })
 export default UserSlice.reducer
-export const { setUsrEmail, setLoading, setFirstPanel, setSecondPanel, setCredentials }  = UserSlice.actions
+export const { setUsrEmail, setLoading, setFirstPanel, setSecondPanel, setSecondPanel2, setCredentials, setFirstPanel2 }  = UserSlice.actions
 
