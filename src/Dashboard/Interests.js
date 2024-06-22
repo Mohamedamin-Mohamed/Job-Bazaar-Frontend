0b94945e925a9ba6f13ec8558ec99c2d96616ff9
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-06-22 09:41:12 -0500

    feat: component to let users set their career interests

---
+++ src/Dashboard/Interests.js
@@ -0,0 +1,15 @@
+
+const Interests = ()=>{
+    return(
+        <div className="flex w-full h-[113px] bg-[#f7f7f7] mt-10">
+            <div className="flex flex-col p-6">
+                <h1 className="mb-4 font-medium">Take 15 mins to set your career interests</h1>
+                <p className="text-sm">Tell us what you want to achieve in your career, and we'll put a plan together for you.</p>
+            </div>
+            <div className="flex justify-center items-center">
+                <button className="bg-[#ffde00] text-black w-[206px] h-[36px] font-medium mr-4">Set Your Career Interests</button>
+            </div>
+        </div>
+    )
+}
+export default Interests
