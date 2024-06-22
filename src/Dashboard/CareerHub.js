8fecc18c491026ca63255f3002e9cef566c0b5f5
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-06-22 09:43:36 -0500

    feat: outputs all of the dashboard components

--- src/Dashboard/CareerHub.js
+++ src/Dashboard/CareerHub.js
@@ -1,12 +1,29 @@
 import NavBar from "./NavBar";
-import ColorGrid from "./ColorGrid";
+import Ribbon from "./Ribbon";
+import Activity from "./Activity";
+import Tasks from "./Tasks";
+import Interests from "./Interests";
+import Explore from "./Explore";
+import {useMediaQuery} from "react-responsive";
 
-const Home = ()=>{
+const CareerHub = ()=>{
+    const isMediumScreen = useMediaQuery({ minWidth:998 }); // Set the breakpoint for md screens
+
     return(
         <>
           <NavBar />
-          <ColorGrid />
+          <Ribbon />
+            <div className="flex gap-x-6">
+                <div className="flex flex-col">
+                    <Tasks />
+                    <Activity />
+                </div>
+                <div className="flex flex-col">
+                   <Interests />
+                    <Explore />
+                </div>
+            </div>
         </>
     )
 }
-export default Home
+export default CareerHub
