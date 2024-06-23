baf4654cb26424b7935226a3ff912a084170f430
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-06-23 09:07:20 -0500

    feat: component to show the ribbon, colors formed by a triangle

--- src/Dashboard/Ribbon.js
+++ src/Dashboard/Ribbon.js
@@ -1,7 +1,10 @@
 import {useSelector} from "react-redux";
 
 const Ribbon = ()=>{
-    const usr = useSelector(state => state.userInfo)
+    const usrInfo = useSelector(state => state.userInfo)
+    const locationInfo = useSelector(state => state.locationInfo)
+    const abbreviatedName = usrInfo.firstName.substring(0,1) + usrInfo.lastName.substring(0,1)
+
     return(
         <div className="relative mx-20 h-60 flex items-center justify-center overflow-hidden mt-0.5">
             <div className="absolute inset-0 flex">
@@ -9,11 +12,11 @@ const Ribbon = ()=>{
                 <div className="w-1/2 h-full bg-[#367c2b]" style={{clipPath: 'polygon(0 0, 100% 0, 100% 100%'}}></div>
             </div>
             <div className="relative z-10 flex items-center space-x-4">
-                <div className="w-16 h-16 rounded-full bg-[#367c2b] flex items-center justify-center text-white text-xl font-bold">
-                    MM
+                <div className="w-20 h-20 rounded-full bg-[#367c2b] flex items-center justify-center text-white text-2xl font-bold">
+                    {abbreviatedName}
                 </div>
                 <div>
-                    <h1 className="text-2xl font-bold">Hi Mohamedamin</h1>
+                    <h1 className="text-2xl font-bold">Hi {usrInfo.firstName}</h1>
                     <p className="text-[#757575] font-bold text-sm">Welcome to your Career Hub</p>
                 </div>
             </div>
