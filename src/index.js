20849e95cf82eccab778ba8508df6897ee445dd2
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-06-23 09:15:02 -0500

    feat: PersistGate component to be used for the local storage with the redux states

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { Provider } from 'react-redux'
-import Store from './Redux/Store'
-import {GoogleOAuthProvider} from "@react-oauth/google";
+import {Provider} from 'react-redux'
+import { Store, persistor } from './Redux/Store'
+import {PersistGate} from "redux-persist/integration/react";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
-    <Provider store={Store} >
-    <App />
+    <Provider store={Store}>
+        <PersistGate persistor={persistor} loading={null}>
+            <App/>
+        </PersistGate>
     </Provider>
 );
