bbb1549883d5de7c0bdf55f43ddc1f000261f57b
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-06-23 09:12:48 -0500

    feat: slice to hold the values returned by the fetch call to grab the users current location

--- src/Redux/LocationSlice.js
+++ src/Redux/LocationSlice.js
@@ -1,15 +1,22 @@
 import {createSlice} from "@reduxjs/toolkit";
 
 const initialState = {
-    city: '',
-    state: '',
-    error: null
+    city:'',
+    states:'',
+    country:'',
 }
 
 const LocationSlice = createSlice({
     name: 'locationInfo',
-    initialState: initialState
+    initialState: initialState,
     reducers: {
-        setCity
+        setLocationInfo: (state, action) => {
+            const { city, states, country } = action.payload
+            state.city = city
+            state.states = states
+            state.country = country
+        }
     }
 })
+export default LocationSlice.reducer
+export const { setLocationInfo } = LocationSlice.actions
