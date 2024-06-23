52f73e11359eb5215bd5ef84a04cf3954675067a
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-06-23 08:59:41 -0500

    refractor: made the cards in the Explore componenet to have style of flex-col when screen size < md

--- src/Dashboard/CareerHub.js
+++ src/Dashboard/CareerHub.js
@@ -6,22 +6,37 @@ import Interests from "./Interests";
 import Explore from "./Explore";
 import {useMediaQuery} from "react-responsive";
 
-const CareerHub = ()=>{
-    const isMediumScreen = useMediaQuery({ minWidth:998 }); // Set the breakpoint for md screens
-
-    return(
+const CareerHub = () => {
+    const isMediumScreen = useMediaQuery({minWidth: 998}); // Set the breakpoint for md screens
+    const smallScreen = useMediaQuery({minWidth: 200})
+    return (
         <>
-          <NavBar />
-          <Ribbon />
-            <div className="flex gap-x-6">
-                <div className="flex flex-col">
-                    <Tasks />
-                    <Activity />
-                </div>
-                <div className="flex flex-col">
-                   <Interests />
-                    <Explore />
-                </div>
+            <NavBar/>
+            <Ribbon/>
+            <div className={`flex ${isMediumScreen ? "gap-x-6" : "flex-col gap-y-6"} mt-6`}>
+                {isMediumScreen ? (
+                    <>
+                        <div className="flex flex-col">
+                            <Tasks/>
+                            <Activity/>
+                        </div>
+                        <div className="flex flex-col xl:w-[950px]">
+                            <Interests/>
+                                <Explore/>
+                        </div>
+                    </>
+                ) : (
+                    <>
+                        <div className={"flex flex-col justify-center items-center"}>
+                            <Interests/>
+                            <Explore/>
+                        </div>
+                        <div className="flex flex-col justify-center items-center">
+                            <Tasks/>
+                            <Activity/>
+                        </div>
+                    </>
+                )}
             </div>
         </>
     )
