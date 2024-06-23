f211ccba73d9445e90da5e3616f78a3c379c8350
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-06-23 08:50:46 -0500

    feat: added profile route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Signup from "./Authentication/Signup";
 import EmailLookup from "./Authentication/EmailLookup";
 import PasswordReset from "./Authentication/PasswordReset";
 import Home from "./LandingPage/Home";
+import Login2 from "./Authentication/Login";
+import CareerHub from "./Dashboard/CareerHub";
+import Profile from "./Dashboard/Profile";
 
 const router = createBrowserRouter(
         createRoutesFromElements(
@@ -13,6 +16,8 @@ const router = createBrowserRouter(
                 <Route index path="accounts/login/email-lookup" element={<EmailLookup />} />
                 <Route path="accounts/login/password-reset" element={<PasswordReset />} />
                 <Route path="accounts/signup" element={<Signup />} />
+                <Route path="/careerhub" element={<CareerHub />} />
+                <Route path="/careerhub/profile" element={<Profile />} />
             </Route>
 
         )
