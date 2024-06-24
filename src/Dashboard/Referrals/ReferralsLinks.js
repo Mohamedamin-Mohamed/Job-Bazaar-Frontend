cc571d8c7510bc5337756084b0310037d223de67
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-06-24 11:55:04 -0500

    feat: outputs the options the users can take to refer other people

---
+++ src/Dashboard/Referrals/ReferralsLinks.js
@@ -0,0 +1,57 @@
+import {NavLink} from "react-router-dom";
+import { IoLogoLinkedin } from "react-icons/io";
+import { FaFacebookSquare } from "react-icons/fa";
+import { RiTwitterXLine } from "react-icons/ri";
+import { IoMail } from "react-icons/io5";
+import { FaCopy } from "react-icons/fa";
+import {FaC} from "react-icons/fa6";
+import { ToastContainer, toast } from 'react-toastify'
+import {useMediaQuery} from "react-responsive";
+import {useEffect, useRef} from "react";
+
+const ReferralsLinks = ({ handleLinksShow })=>{
+    const isMediumScreen = useMediaQuery({minWidth: 998}); // Set the breakpoint for md screens
+    const ref = useRef(null)
+    const handleCopy = ()=>{
+        toast.success("Job link has been copied to clipboard")
+    }
+    useEffect(() => {
+        const handleClickOutside = (event) =>{
+            if(ref.current && !ref.current.contains(event.target)){
+                handleLinksShow()
+            }
+        }
+        document.addEventListener('mousedown', handleClickOutside)
+        return()=>{
+            document.removeEventListener('mousedown', handleClickOutside)
+        }
+    }, [handleLinksShow]);
+    return(
+        <div className="flex justify-center items-center top-3/4 ml-80 mt-4 absolute h-[238px] bg-white p-2 border ease-in-out " ref={ref}>
+            <ToastContainer position="top-center" />
+            <nav>
+                <div className="flex p-2 space-x-2 text-[#4f5666] h-10 mt-1.5 rounded border-2 border-black">
+                    <IoLogoLinkedin size={25} />
+                    <NavLink to="https://www.linkedin.com">Refer via LinkedIn</NavLink>
+                </div>
+                <div className="flex p-2.5 space-x-2 text-[#4f5666]">
+                    <FaFacebookSquare size={25} />
+                    <NavLink to="https://www.facebook.com">Refer via Facebook</NavLink>
+                </div>
+                <div className="flex p-2.5 space-x-2 text-[#4f5666]">
+                    <RiTwitterXLine size={25} />
+                    <NavLink to="https://www.x.com">Refer via X (Formerly Twitter) </NavLink>
+                </div>
+                <div className="flex p-2.5 space-x-2 text-[#4f5666]">
+                    <IoMail size={25} />
+                    <NavLink to="mailto:">Refer via Email</NavLink>
+                </div>
+                <div className="flex p-2.5 space-x-2 text-[#4f5666]">
+                    <FaCopy size={25} />
+                    <NavLink to="" onClick={handleCopy}>Copy Link</NavLink>
+                </div>
+            </nav>
+        </div>
+    )
+}
+export default ReferralsLinks
