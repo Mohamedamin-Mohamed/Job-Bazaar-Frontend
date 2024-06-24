64370238f89e959b2db18a0b3992affe70069b9a
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-06-24 11:52:21 -0500

    feat: component to showcase the recommendation of the user

---
+++ src/Dashboard/Profile/Recommendations.js
@@ -0,0 +1,21 @@
+const Recommendations = ()=>{
+    return(
+        <>
+            <div>
+                <h1>Make Career Hub work for you</h1>
+                <p>Add more to your profile and get recommendations for the career you want</p>
+                <h1>Profile Completeness</h1>
+                <div className="flex mx-6 mb-4">
+                    <div className="w-[65px] h-[6px] bg-[#367c2b] rounded-md mr-1"></div>
+                    <div className="w-[65px] h-[6px] bg-[#367c2b] rounded-md mr-1"></div>
+                    <div className="w-[65px] h-[6px] bg-[#367c2b] rounded-md mr-1"></div>
+                    <div className="w-[65px] h-[6px] bg-[#367c2b] rounded-md"></div>
+                    <p>2/4</p>
+                </div>
+
+            </div>
+        </>
+
+    )
+}
+export default Recommendations
