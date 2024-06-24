834bbf6d0da63dc981b24f8ce9a8f4cd27c91497
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-06-24 11:48:36 -0500

    fix: changed the link to refer a friend

--- src/Dashboard/Activity.js
+++ src/Dashboard/Activity.js
@@ -6,19 +6,19 @@ const Activity = ()=>{
     return(
         <div className={`"flex flex-col text-[#367c2b] mt-10 border ${!isMediumScreen ? "w-[88%] mx-12 p-4" : "flex w-[345px] ml-28 p-4"} h-[280px]"`}>
             <h1 className="text-black font-bold p-1.5">My activity</h1>
-           <div className="flex justify-between p-2 font-bold">
+           <div className="flex justify-between p-2 font-bold hover:underline hover:cursor-pointer">
                <p>Job Applications</p>
                <p className="bg-[#cfd9cc] rounded-md px-1">0</p>
            </div>
-            <div className="flex justify-between p-2 font-bold">
+            <div className="flex justify-between p-2 font-bold hover:underline hover:cursor-pointer">
                 <p>Project Applications</p>
                 <p className="bg-[#cfd9cc] rounded-md px-1">0</p>
             </div>
-            <div className="flex justify-between p-2 border-b mb-4 pb-6 font-bold">
+            <div className="flex justify-between p-2 border-b mb-4 pb-6 font-bold hover:underline hover:cursor-pointer">
                 <p>Referrals</p>
                 <p className="bg-[#cfd9cc] rounded-md px-1">0</p>
             </div>
-            <div className="flex justify-center font-bold">
+            <div className="flex justify-center font-bold hover:underline hover:cursor-pointer">
                 <NavLink to='refer'>Refer a friend</NavLink>
             </div>
         </div>
