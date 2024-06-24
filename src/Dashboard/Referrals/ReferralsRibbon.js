a337d27d342ba2f9c8e3d898c45c24c5d7b48eaa
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-06-24 11:56:07 -0500

    fix: now the destructuring of the props will contain a height variable

---
+++ src/Dashboard/Referrals/ReferralsRibbon.js
@@ -0,0 +1,25 @@
+import {MdLocationOn} from "react-icons/md";
+import Image from '../../Images/referrals.png'
+import {useNavigate} from "react-router-dom";
+import NavBar from "../NavBar";
+const ReferralsRibbon = ({ text, height })=>{
+    return(
+        <>
+            <div>
+                <NavBar />
+                <div className={`relative mx-10 h-${height} flex overflow-hidden mt-0.5`}>
+                    <div className="absolute inset-0 flex">
+                        <div className="w-1/2 h-full bg-[#ffde00]"
+                             style={{clipPath: 'polygon(0 0, 100% 0, 0 100%'}}></div>
+                        <div className="w-1/2 h-full bg-[#367c2b]"
+                             style={{clipPath: 'polygon(0 0, 100% 0, 100% 100%'}}></div>
+                    </div>
+                    <div className="ml-6 mt-14 z-50">
+                        <h1 className="text-3xl font-mono font-bold">{ text }</h1>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+export default ReferralsRibbon
