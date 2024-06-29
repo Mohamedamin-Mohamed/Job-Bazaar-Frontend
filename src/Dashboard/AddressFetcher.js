0cae352af9ee8fb309ea87feeada08bd397a5e6b
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-06-29 10:56:34 -0500

    refractor: removed the unnecessary fields in the address object

--- src/Dashboard/AddressFetcher.js
+++ src/Dashboard/AddressFetcher.js
@@ -11,9 +11,9 @@ const AddressFetcher = ({latitude, longitude, onError}) => {
                 const response = await fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}
                &lon=${longitude}`);
                 const data = await response.json()
-
+                console.log(data)
                 let address = {
-                    city: data.address.city || data.address.town || data.address.village,
+                    city: data.address.city,
                     states: data.address.state,
                     country: data.address.country
                 }
