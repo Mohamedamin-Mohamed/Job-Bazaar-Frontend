2121dfa9242ad68bcbe17f6f5e77c881869e855c
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-07-05 10:18:11 -0500

    feat: components that outputs the components of its parent directory

--- src/Dashboard/Settings/Settings.js
+++ src/Dashboard/Settings/Settings.js
@@ -1,32 +1,19 @@
-import ReferralsRibbon from "./Referrals/ReferralsRibbon";
-import { BiSolidUserAccount } from "react-icons/bi";
+import ReferralsRibbon from "../Referrals/ReferralsRibbon";
+import {BiSolidUserAccount} from "react-icons/bi";
 import {useState} from "react";
+import Account from "./Account";
+import Activity from "./Activity";
+import JobAlerts from "./JobAlerts";
 
-const Settings = ()=>{
-    const[termsAndServices, setTermsAndServices] = useState(false)
-    return(
-        <div>
-            <ReferralsRibbon text={"Settings"} height={60} />
-            <div className="flex justify-center items-center absolute inset-0 w-full h-screen">
-            <div className="flex flex-col justify-center pl-4 w-[520px] h-[200px] border">
-                <div className="flex space-x-2">
-                    <BiSolidUserAccount size={20} color="gray" className="mt-2" />
-                    <h1 className="font-semibold text-lg">Account Settings</h1>
-                </div>
-                <h1 className="text-gray-500 pb-6">Manage your account and personal information</h1>
-                <div>
-                    <div className="flex flex-col border-t py-4">
-                        <div className="flex">
-                            <h1 className="font-semibold">Terms and Services</h1>
-                            <p className="ml-auto mr-4 cursor-pointer text-[#367c2b]">View</p>
-                        </div>
-                        <h1 className="text-gray-500">Review Terms and Services you agreed to in onboarding</h1>
-                    </div>
-                </div>
-
-            </div>
-            </div>
-        </div>
+const Settings = () => {
+    const [termsAndServices, setTermsAndServices] = useState(false)
+    return (
+        <>
+            <ReferralsRibbon text={"Settings"} height={60}/>
+            <Account/>
+            <Activity/>
+            <JobAlerts/>
+        </>
     )
 }
 export default Settings
