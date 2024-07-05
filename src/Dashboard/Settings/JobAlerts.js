8014996144d8996d43f191f7725b8ed2d4eca3c3
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-07-05 10:17:36 -0500

    feat: new component that showcases current job alets of the user

---
+++ src/Dashboard/Settings/JobAlerts.js
@@ -0,0 +1,26 @@
+import {IoIosSearch} from "react-icons/io";
+import Image from '../../Images/empty_self_chat.svg'
+import {useNavigate} from "react-router-dom";
+const JobAlerts = ()=>{
+    const navigate = useNavigate()
+    const handleNavigation = ()=>{
+        navigate('/careerhub/explore/jobs')
+    }
+    return(
+          <div className="flex justify-center items-center mt-8">
+              <div className="flex flex-col justify-center pl-4 md:w-[840px] w-[450px] h-[460px] border mb-10">
+                  <div className="flex ml-2">
+                      <IoIosSearch size={20} color="gray" className="mt-1.5"/>
+                      <h1 className="font-semibold text-xl ml-2">Job alerts you created</h1>
+                  </div>
+                  <p className="border-b pb-8 ml-3 text-gray-500">Emails you receive about new job postings that match these searches.</p>
+                  <div className="flex flex-col justify-center items-center">
+                      <img src={Image} alt="" className="h-[200px]"/>
+                      <p className="my-4">No job searches saved yet</p>
+                      <button className="text-[#367c2b] bg-[#e6f0e1] border px-3 py-2 font-semibold rounded" onClick={handleNavigation}>Search Jobs</button>
+                  </div>
+              </div>
+      </div>
+    )
+}
+export default JobAlerts
