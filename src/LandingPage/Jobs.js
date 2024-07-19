0ffb5349fd7de0519b79a430f90128d78bfce92a
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-07-19 18:36:16 -0500

    feat: changed background color of the button that navigates to the login screen

--- src/LandingPage/Jobs.js
+++ src/LandingPage/Jobs.js
@@ -7,7 +7,7 @@ const Jobs = ({setJobs})=>{
             <div className="border-l border-b w-[28%] h-[220px] pl-4 pt-4">
                 <p className="font-medium">Find the right job</p>
                 <p>Millions of jobs. Search by what matters to you and find the one that's right for you.</p>
-                <button className="border border-gray-500 mt-8  ml-10 p-2.5 rounded-md hover:bg-blue-600" onClick={()=> navigate('/accounts/login')}>Start using Job Bazaar</button>
+                <button className="border border-gray-500 mt-8  ml-10 p-2.5 rounded-md hover:bg-[#00A264]" onClick={()=> navigate('/accounts/login')}>Start using Job Bazaar</button>
             </div>
             <div className="border-r border-b h-[220px]">
                 <img src={CommunityImage} alt=""/>
