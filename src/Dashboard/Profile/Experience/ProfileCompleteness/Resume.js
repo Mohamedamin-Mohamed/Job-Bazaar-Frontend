a9f77eea83de564f235255bd74a2d77bac683c29
Mohamedamin Mohamed <MohamedMohamedamin@JohnDeere.com>
2024-07-19 18:25:47 -0500

    feat: new component thats lets users to complete profile with their resume

--- src/Dashboard/Profile/Experience/ProfileCompleteness/Resume.js
+++ src/Dashboard/Profile/Experience/ProfileCompleteness/Resume.js
@@ -1,17 +1,17 @@
 import Image from "../../../../Images/resume.svg";
 
-const ResumeUpload = ()=>{
+const Resume = ()=>{
     return (
-        <div className="flex flex-col w-[350px] h-[210px] border p-3 my-4">
-            <div className="flex">
-                <h1 className="text-lg font-semibold w-[60%]">Complete profile with resume</h1>
+        <div className="flex flex-col h-[230px] border p-3 my-4">
+            <div className="flex w-[350px]">
+                <h1 className="text-lg font-semibold w-[64%] text-wrap">Complete profile with resume</h1>
                 <img src={Image} alt="" className="w-[82px] h-[90px] ml-auto"/>
             </div>
             <div>
-                <p className="w-[70%] mb-2">Add your career information in one step.</p>
-                <button className="bg-[#ffde00] w-[140px] h-[36px] rounded-sm">Upload Resume</button>
+                <p className="w-[78%] mb-2">Add your career information in one step.</p>
+                <button className="bg-[#ffde00] w-[140px] h-[36px] rounded-sm mt-11">Upload Resume</button>
             </div>
         </div>
     )
 }
-export default ResumeUpload
+export default Resume
