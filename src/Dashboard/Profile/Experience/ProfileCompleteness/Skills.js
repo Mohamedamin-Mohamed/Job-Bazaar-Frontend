c329cdc8940ca0338b951967b99f8d00355c3048
Mohamedamin Mohamed <MohamedMohamedamin@JohnDeere.com>
2024-07-19 18:26:18 -0500

    feat: new component that lets users to add their skills

--- src/Dashboard/Profile/Experience/ProfileCompleteness/Skills.js
+++ src/Dashboard/Profile/Experience/ProfileCompleteness/Skills.js
@@ -1,17 +1,16 @@
-import Image from "../../../../Images/resume.svg";
-
-const ShowcaseSkills = ()=>{
+import Image from '../../../../Images/skills.svg'
+const Skills = ()=>{
     return (
-        <div className="flex flex-col w-[350px] h-[210px] border p-3 my-4">
-            <div className="flex">
+        <div className="flex flex-col h-[230px] border p-3 my-4">
+            <div className="flex w-[350px]">
                 <h1 className="text-lg font-semibold w-[60%]">Showcase your skills</h1>
-                <img src={Image} alt="" className="w-[82px] h-[90px] ml-auto"/>
+                <img src={Image} alt="" className="w-[82px] h-[70px] ml-auto"/>
             </div>
             <div>
-                <p className="w-[70%] mb-2">Show what you know to your teammates and managers. Get recommendations to grow.</p>
-                <button className="bg-[#ffde00] w-[140px] h-[36px] rounded-sm">Add Skills</button>
+                <p className="w-[90%] text-wrap mb-2">Show what you know to your teammates and managers. Get recommendations to grow.</p>
+                <button className="bg-[#ffde00] w-[140px] h-[36px] rounded-sm mt-4">Add Skills</button>
             </div>
         </div>
     )
 }
-export default ShowcaseSkills
+export default Skills
