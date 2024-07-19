08b89cbe4fe4def7bf07c1d5c9a2b05d5365e16c
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-07-19 18:33:44 -0500

    feat: new component that populates the child components

--- src/Dashboard/Profile/Experience/Experience.js
+++ src/Dashboard/Profile/Experience/Experience.js
@@ -1,31 +1,15 @@
 import {useSelector} from "react-redux";
-import {MdLocationOn} from "react-icons/md";
-import { GoMail } from "react-icons/go";
-import {NavLink} from "react-router-dom";
+import ContactLinks from "./ContactLinks";
+import ProfileCompleteness from "./ProfileCompleteness/ProfileCompleteness";
+import Education from "./Education/Education";
 
 const Experience = ()=>{
-    const usrInfo =  useSelector(state => state.userInfo)
-    const locationInfo = useSelector(state => state.locationInfo)
-    const capitalize = ()=>{
-        return usrInfo.usrEmail.charAt(0).toUpperCase() + usrInfo.usrEmail.slice(1);
-    }
 
     return(
         <div className="">
-            <div className="flex flex-col justify-center pl-6 md:w-[880px] mx-4 w-[450px] h-[120px] border">
-               <h1 className="text-xl font-semibold mb-4">Contact & Links</h1>
-                <div className="flex mb-1.5">
-                    <MdLocationOn size={20} className="mr-4" color="gray" />
-                    <p>{locationInfo.city },</p>
-                    <p>{locationInfo.states}</p>
-                    <p>{locationInfo.country}</p>
-                </div>
-                <div className="flex space-x-4">
-                    <GoMail size={20} color="gray" />
-                    <NavLink to="mailto:mohamedamin204080@gmail.com" className="text-[#367c2b]">{ capitalize }</NavLink>
-                </div>
-            </div>
-
+           <ContactLinks />
+            <ProfileCompleteness />
+            <Education />
         </div>
     )
 }
