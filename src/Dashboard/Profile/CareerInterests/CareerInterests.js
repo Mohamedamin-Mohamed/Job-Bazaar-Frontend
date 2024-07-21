d0c085162da6939bbcd20e19a4e5d16ad506f321
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-07-20 19:35:18 -0500

    fix: changed the layout to display the components based on the media screen

---
+++ src/Dashboard/Profile/CareerInterests/CareerInterests.js
@@ -0,0 +1,33 @@
+import Skills from "./Skill/Skills";
+import Roles from "./Role/Roles";
+import RoleBased from "./Role/RoleBased";
+import SkillBased from "./Skill/SkillBased";
+import {useMediaQuery} from "react-responsive";
+
+const CareerInterests = () => {
+    const mediaQuery = useMediaQuery({minWidth: "1200px"})
+    return (
+        <>
+            {mediaQuery ?
+                <div className="flex justify-center items-center">
+                <div>
+                    <Skills/>
+                    <Roles/>
+                </div>
+                    <div>
+                        <RoleBased/>
+                        <SkillBased/>
+                    </div>
+                </div>
+                :
+                <div>
+                    <Skills />
+                    <Roles />
+                    <RoleBased />
+                    <SkillBased />
+                </div>
+            }
+        </>
+    )
+}
+export default CareerInterests
