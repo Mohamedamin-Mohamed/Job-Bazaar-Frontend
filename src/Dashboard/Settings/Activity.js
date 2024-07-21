6b88e74188230c10bf0fee69b08d96e50f43c0c4
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-07-21 13:05:55 -0500

    refractor: removed unused imports

--- src/Dashboard/Settings/Activity.js
+++ src/Dashboard/Settings/Activity.js
@@ -1,72 +1,70 @@
-import {BiSolidUserAccount} from "react-icons/bi";
-import { LuTimerReset } from "react-icons/lu";
+import {LuTimerReset} from "react-icons/lu";
 import {useEffect, useRef, useState} from "react";
-import { MdKeyboardArrowUp } from "react-icons/md";
-import { ToastContainer, toast } from 'react-toastify'
+import {MdDelete, MdKeyboardArrowUp} from "react-icons/md";
+import {toast, ToastContainer} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import { MdDelete } from "react-icons/md";
 
-const Activity = ()=>{
-    const[text, setText] = useState("Off")
-    const[toggle, setToggle] = useState(false)
+const Activity = () => {
+    const [text, setText] = useState("Off")
+    const [toggle, setToggle] = useState(false)
     const ref = useRef(null)
 
-    const handleText = (text)=>{
+    const handleText = (text) => {
         setText(text)
         setToggle(false)
         toast.success('Your personalization has been set.')
     }
-    const handleToggle = ()=>{
-        setToggle(false)
-    }
-    const handleDelete = ()=>{
+
+    const handleDelete = () => {
         toast.success('Activity has been triggered')
     }
     useEffect(() => {
-        const handleClickOutside = (event)=>{
-            if(ref.current && !ref.current.contains(event.target)){
-                handleToggle()
+        const handleClickOutside = (event) => {
+            if (ref.current && !ref.current.contains(event.target)) {
+                setToggle(false)
             }
-            document.addEventListener('mousedown', handleClickOutside)
-            return(()=>{
-                document.removeEventListener('mousedown', handleClickOutside)
-            })
         }
-    }, [handleToggle]);
-    return(
+        document.addEventListener('mousedown', handleClickOutside)
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside)
+        }
+    }, [toggle]);
+    return (
         <div className="flex justify-center items-center mt-8">
-            <ToastContainer position="top-center" />
-        <div className="flex flex-col justify-center mx-2 pl-4 md:w-[840px] w-[450px] h-[300px] border">
-            <div className="flex space-x-2">
-                <LuTimerReset size={20} color="gray" className="mt-2"/>
-                <h1 className="font-semibold text-lg">Activity based personalization settings</h1>
-            </div>
-            <p className="text-gray-500 ml-1 pb-4 border-b">Manage your activity based personalization</p>
-            <div className="flex">
-                <div className="flex flex-col pt-4">
-                    <h1 className="font-semibold">Enable user activity based personalization</h1>
-                    <p className="text-gray-500">Turn recommendations based on your activity on or off</p>
+            <ToastContainer position="top-center"/>
+            <div className="flex flex-col justify-center mx-2 pl-4 md:w-[840px] w-[450px] h-[300px] border">
+                <div className="flex space-x-2">
+                    <LuTimerReset size={20} color="gray" className="mt-2"/>
+                    <h1 className="font-semibold text-lg">Activity based personalization settings</h1>
                 </div>
-                <div className="flex justify-center items-center bg-[#ffde00] w-[72px] h-[36px] text-center font-semibold mr-6 mt-2 cursor-pointer ml-auto" onClick={()=> setToggle(!toggle)}>
-                    {text}
-                    <MdKeyboardArrowUp size={25}/>
-                </div>
-                {toggle && (
-                    <div className="absolute md:ml-[726px] md:top-[494px] top-[542px] ml-[209px] flex flex-col justify-start mt-[224px] border w-[200px] h-[90px] p-2 bg-white" ref={ref}>
-                        <button className="flex justify-start p-1" onClick={ ()=> handleText('Off')}>Off</button>
-                        <button className="flex justify-start p-1" onClick={()=> handleText('On')}>On</button>
+                <p className="text-gray-500 ml-1 pb-4 border-b">Manage your activity based personalization</p>
+                <div className="flex" ref={ref}>
+                    <div className="flex flex-col pt-4">
+                        <h1 className="font-semibold">Enable user activity based personalization</h1>
+                        <p className="text-gray-500">Turn recommendations based on your activity on or off</p>
                     </div>
+                    <div
+                        className="flex justify-center items-center bg-[#ffde00] w-[72px] h-[36px] text-center font-semibold mr-6 mt-2 cursor-pointer ml-auto"
+                        onClick={() => setToggle(!toggle)}>
+                        <span>{text}</span>
+                        <MdKeyboardArrowUp size={25}/>
+                    </div>
+                        {toggle && (
+                            <div className="absolute md:ml-[726px] md:top-[494px] top-[542px] ml-[209px] flex flex-col justify-start mt-[224px] border w-[200px] h-[90px] p-2 bg-white">
+                            <button className="flex justify-start p-1" onClick={() => handleText('Off')}>Off</button>
+                            <button className="flex justify-start p-1" onClick={() => handleText('On')}>On</button>
+                            </div>
                 )}
             </div>
             <div className="flex flex-col my-6">
                 <h1 className="font-semibold">Delete Activity</h1>
                 <div className="flex">
-                    <p className="text-gray-500">Clear your activity for personalization</p>
-                    <MdDelete size={20} className="ml-auto mr-6 cursor-pointer" onClick={handleDelete}/>
-                </div>
+                        <p className="text-gray-500">Clear your activity for personalization</p>
+                        <MdDelete size={20} className="ml-auto mr-6 cursor-pointer" onClick={handleDelete}/>
+                    </div>
 
+                </div>
             </div>
-        </div>
         </div>
     )
 }
