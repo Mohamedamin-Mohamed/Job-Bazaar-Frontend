2d38f8bce36533098280619fb48b64e196cdddec
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-07-20 19:35:57 -0500

    feat: a generic component that displays skill or role based suggestions

---
+++ src/Dashboard/Profile/CareerInterests/Suggestions.js
@@ -0,0 +1,31 @@
+import { VscClose } from "react-icons/vsc";
+import {useEffect, useRef} from "react";
+
+const Suggestions = ({open, handleOpen, title, text})=>{
+    const ref = useRef(null)
+
+    useEffect(() => {
+        const handleClickOutside = (event)=>{
+            if(ref.current && !ref.current.contains(event.target)){
+                handleOpen()
+            }
+        }
+        document.addEventListener('mousedown', handleClickOutside)
+        return ()=>{
+            document.removeEventListener('mousedown', handleClickOutside)
+        }
+    }, [handleOpen]);
+    return(
+        <div className={!open ? 'hidden' : 'fixed flex justify-center inset-0 items-center text-black backdrop-brightness-50 z-50'}>
+            <div className="flex flex-col p-7 text-black bg-white w-[516px] border h-[220px] ease-in-out duration-500" ref={ref}>
+                <div className="flex">
+                    <h1 className="text-lg font-semibold mb-6">{title} based suggestions</h1>
+                    <VscClose size={22} className="ml-auto cursor-pointer" onClick={handleOpen}/>
+                </div>
+                <p className="text-[#4f5666] mb-6">Projects and courses that will help you {text} you're interested in.</p>
+                <p className="font-semibold">0 suggestions(s)</p>
+            </div>
+        </div>
+    )
+}
+export default Suggestions
