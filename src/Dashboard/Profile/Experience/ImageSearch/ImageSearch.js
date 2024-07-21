a7e9029cea6244227e8f24b19c6532d534439d2b
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-07-20 19:28:59 -0500

    feat: added component that scraps image from the google results and returns 10 of the images

--- src/Dashboard/Profile/Experience/ImageSearch/ImageSearch.js
+++ src/Dashboard/Profile/Experience/ImageSearch/ImageSearch.js
@@ -1,18 +1,21 @@
 import {useEffect} from "react";
 
-const ImageSearch = ({schoolName, handleImageUrl, onError}) => {
-    const apiKey = 'AIzaSyA2DL6D565P86EeuOZwe3n1en_dXNkH9yk'
-    const cx = '43a775949f03f427d'
+const ImageSearch = ({query, handleImageUrl, onError}) => {
+    const env = require('dotenv')
+
+    const apiKey = process.env.API_KEY
+    const cx = process.env.SEARCH_ENGINE_ID
 
     useEffect(() => {
         const abortController = new AbortController();
         const signal = abortController.signal
         const fetchImage = async () => {
             try {
-                const response = await fetch(`https://www.googleapis.com/customsearch/v1?q=${encodeURIComponent(schoolName)}&cx=${cx}&key=${apiKey}&searchType=image`, {signal})
+                const response = await fetch(`https://www.googleapis.com/customsearch/v1?q=${encodeURIComponent(query)}&cx=${cx}&key=${apiKey}&searchType=image`, {signal})
                 if (!response.ok) {
                     throw new Error('Failed to fetch image')
                 }
+                console.log('Here is the data')
                 const data = await response.json()
                 if (data.items && data.items.length > 0) {
                     handleImageUrl(data.items[5].link)
@@ -27,6 +30,6 @@ const ImageSearch = ({schoolName, handleImageUrl, onError}) => {
         return ()=>{
             abortController.abort("fetch request finished")
         }
-    }, [schoolName]);
+    }, [query]);
 }
 export default ImageSearch
