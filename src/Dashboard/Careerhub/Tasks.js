16d2add90d776c156a7cc56cf9f99c0c359af8eb
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-07-20 19:42:27 -0500

    fix: changed the height of the outermost component

--- src/Dashboard/Careerhub/Tasks.js
+++ src/Dashboard/Careerhub/Tasks.js
@@ -1,13 +1,14 @@
-import { IoIosArrowRoundForward } from "react-icons/io";
-import { TbBulb } from "react-icons/tb";
-import { CiFilter } from "react-icons/ci";
+import {IoIosArrowRoundForward} from "react-icons/io";
+import {TbBulb} from "react-icons/tb";
+import {CiFilter} from "react-icons/ci";
 import {useMediaQuery} from "react-responsive";
 
-const Tasks = ()=>{
+const Tasks = () => {
     const isMediumScreen = useMediaQuery({minWidth: 998}); // Set the breakpoint for md screens
 
-    return(
-        <div className={`"flex h-screen flex-col text-[#367c2b] mt-10 border ${!isMediumScreen ? "w-[88%] mx-12 p-4 relative" : "flex w-[345px] px-4 pt-3.5"} h-[257px] "`}>
+    return (
+        <div
+            className={`"flex h-screen flex-col text-[#367c2b] mt-10 border ${!isMediumScreen ? "w-[88%] mx-12 p-4 relative" : "flex w-[345px] px-4 pt-3.5"} h-[250px] "`}>
             <div className="flex justify-between mb-4">
                 <div className="flex">
                     <h1 className="mr-2 text-black font-bold">My Tasks </h1>
@@ -18,7 +19,8 @@ const Tasks = ()=>{
             </div>
             <div className="flex">
                 <IoIosArrowRoundForward size={25} color="green"/>
-                <p className="ml-4 mb-4 text-black font-semibold hover:underline hover:cursor-pointer">Update your Profile</p>
+                <p className="ml-4 mb-4 text-black font-semibold hover:underline hover:cursor-pointer">Update your
+                    Profile</p>
             </div>
             <div className="flex  mb-4">
                 <div className="md:w-[300px] w-[65px] h-[6px] bg-[#367c2b] rounded-md mr-1"></div>
@@ -28,7 +30,7 @@ const Tasks = ()=>{
             </div>
             <div className="flex justify-between">
                 <div className="flex bg-[#fff89c] ">
-                    <TbBulb size={16} className="mt-0.5" />
+                    <TbBulb size={16} className="mt-0.5"/>
                     <p className="text-[#5c5500] font-medium">Improves recommendations</p>
                 </div>
                 <p className="text-[#4f5666">0/4 steps</p>
@@ -36,15 +38,17 @@ const Tasks = ()=>{
 
             <div className="flex mt-4">
                 <IoIosArrowRoundForward size={25} color="green"/>
-                <p className="ml-4 mb-4 text-black font-semibold hover:underline hover:cursor-pointer">Update your Career Interests</p>
+                <p className="ml-4 mb-4 text-black font-semibold hover:underline hover:cursor-pointer">Update your
+                    Career Interests</p>
             </div>
             <div className="flex mb-4 ">
-                <div className="md:w-[500px] w-[130px] h-[6px] bg-[#e6f0e1] rounded-md mr-1 border border-green-700"></div>
+                <div
+                    className="md:w-[500px] w-[130px] h-[6px] bg-[#e6f0e1] rounded-md mr-1 border border-green-700"></div>
                 <div className="md:w-[500px] w-[127px] h-[6px] bg-[#e6f0e1] rounded-md  border  border-green-700"></div>
             </div>
             <div className="flex justify-between">
                 <div className="flex bg-[#fff89c] p-0.5">
-                    <CiFilter size={16} color="black" className="mt-0.5" />
+                    <CiFilter size={16} color="black" className="mt-0.5"/>
                     <p className="text-[#5c5500] font-medium">Improves personalization</p>
                 </div>
                 <p className="text-[#4f5666">0/2 steps</p>
