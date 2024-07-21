2a7297f04e38e026d9228d182598502b50b78780
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-07-21 13:03:40 -0500

    feat: added an event listener to the component that listens for clicks outside the componenet and closes it if there are any clicks

--- src/Dashboard/Profile/CareerInterests/Suggestions.js
+++ src/Dashboard/Profile/CareerInterests/Suggestions.js
@@ -20,7 +20,7 @@ const Suggestions = ({open, handleOpen, title, text})=>{
             <div className="flex flex-col p-7 text-black bg-white w-[516px] border h-[220px] ease-in-out duration-500" ref={ref}>
                 <div className="flex">
                     <h1 className="text-lg font-semibold mb-6">{title} based suggestions</h1>
-                    <VscClose size={22} className="ml-auto cursor-pointer" onClick={handleOpen}/>
+                    <VscClose size={22} className="ml-auto cursor-pointer hover:rounded-full hover:border hover:bg-gray-100" onClick={handleOpen}/>
                 </div>
                 <p className="text-[#4f5666] mb-6">Projects and courses that will help you {text} you're interested in.</p>
                 <p className="font-semibold">0 suggestions(s)</p>
