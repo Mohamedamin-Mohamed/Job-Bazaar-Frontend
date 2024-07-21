05fa635f0f8df3c5111bb2f73fd3362f408b2902
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-07-20 19:39:18 -0500

    feat: added value municipality to the city key

--- src/Dashboard/Address/AddressFetcher.js
+++ src/Dashboard/Address/AddressFetcher.js
@@ -1,19 +1,23 @@
 import {useEffect} from "react";
 import {useDispatch} from "react-redux";
 import {setLocationInfo} from "../../Redux/LocationSlice";
+import {retry} from "@reduxjs/toolkit/query";
 
 const AddressFetcher = ({latitude, longitude, onError}) => {
     const dispatch = useDispatch()
 
     useEffect(() => {
+        const abortController = new AbortController()
+        const signal = abortController.signal
+
         const fetchAddress = async () => {
             try {
                 const response = await fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}
-               &lon=${longitude}`);
+               &lon=${longitude}`, {signal});
                 const data = await response.json()
                 console.log(data)
                 let address = {
-                    city: data.address.city || data.address.village || data.address.town,
+                    city: data.address.city || data.address.village || data.address.town || data.address.municipality,
                     states: data.address.state,
                     country: data.address.country
                 }
@@ -23,6 +27,9 @@ const AddressFetcher = ({latitude, longitude, onError}) => {
             }
         }
         fetchAddress()
+        return ()=>{
+            abortController.abort("component unmounted or dependency changed")
+        }
     }, [latitude, longitude, dispatch, onError]);
     return null;
 }
