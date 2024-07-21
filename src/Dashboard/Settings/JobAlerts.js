e5dee52dfbdba1436aa384fd130a912cfb9e0c4b
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-07-20 19:45:28 -0500

    feat: added new function that handles navigation from the page

--- src/Dashboard/Settings/JobAlerts.js
+++ src/Dashboard/Settings/JobAlerts.js
@@ -11,13 +11,13 @@ const JobAlerts = ()=>{
               <div className="flex flex-col justify-center pl-4 md:w-[840px] mx-2 w-[450px] h-[460px] border mb-10">
                   <div className="flex ml-2">
                       <IoIosSearch size={20} color="gray" className="mt-1.5"/>
-                      <h1 className="font-semibold text-xl ml-2">Job alerts you created</h1>
+                      <h1 className="font-semibold text-xl ml-2 mb-1">Job alerts you created</h1>
                   </div>
                   <p className="border-b pb-8 ml-3 text-gray-500">Emails you receive about new job postings that match these searches.</p>
                   <div className="flex flex-col justify-center items-center">
                       <img src={Image} alt="" className="h-[200px]"/>
                       <p className="my-4">No job searches saved yet</p>
-                      <button className="text-[#367c2b] bg-[#e6f0e1] border px-3 py-2 font-semibold rounded" onClick={handleNavigation}>Search Jobs</button>
+                      <button className="text-[#367c2b] bg-[#e6f0e1] border px-3 py-2 font-semibold rounded hover:bg-[#367c2b] hover:text-white" onClick={handleNavigation}>Search Jobs</button>
                   </div>
               </div>
       </div>
