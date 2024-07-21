fa38fefe4c0cc7461fd25555bf0b6e38b613794b
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-07-20 19:43:13 -0500

    feat: added new component TermsOfService

--- src/Dashboard/Settings/Account.js
+++ src/Dashboard/Settings/Account.js
@@ -1,7 +1,13 @@
-import ReferralsRibbon from "../Referrals/ReferralsRibbon";
 import {BiSolidUserAccount} from "react-icons/bi";
+import {useState} from "react";
+import TermsOfService from "./TermsOfService";
 
 const Account = ()=>{
+    const[open, setOpen] = useState(false)
+
+    const handleOpen = ()=>{
+        setOpen(!open)
+    }
     return(
                 <div className="flex justify-center items-center md:mt-0 mt-16">
                     <div className="flex flex-col justify-center pl-4 md:w-[840px] mx-2 w-[450px] h-[200px] border">
@@ -14,7 +20,8 @@ const Account = ()=>{
                             <div className="flex flex-col border-t py-4">
                                 <div className="flex">
                                     <h1 className="font-semibold">Terms and Services</h1>
-                                    <p className="ml-auto mr-4 cursor-pointer text-[#367c2b]">View</p>
+                                    <p className="ml-auto mr-4 cursor-pointer text-[#367c2b]" onClick={handleOpen}>View</p>
+                                    { open && <TermsOfService open={open} handleOpen={handleOpen} /> }
                                 </div>
                                 <h1 className="text-gray-500">Review Terms and Services you agreed to in onboarding</h1>
                             </div>
