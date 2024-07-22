4c3d3031a7b69043eb754c9cdac5e6b7ee076e1e
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-07-22 16:15:48 -0500

    feat: added an argument to the arguments being received by GetEducation

--- src/Dashboard/Profile/Experience/Education/FetchEducation/GetEducation.js
+++ src/Dashboard/Profile/Experience/Education/FetchEducation/GetEducation.js
@@ -1,5 +1,4 @@
-const GetEducation = async (email)=>{
-    const abortController = new AbortController()
+const GetEducation = async (email, abortController)=>{
     return await fetch(`http://localhost:8080/api/user-education/get/${email}`, {
         signal: abortController.signal
     })
