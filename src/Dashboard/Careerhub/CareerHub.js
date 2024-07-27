5f9b49c6e9e591a678f6f5f51a9ad103d15fd6d8
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-07-27 11:03:14 -0500

    feat: addition of checking if the current token is valid, if not redirect the user to login

--- src/Dashboard/Careerhub/CareerHub.js
+++ src/Dashboard/Careerhub/CareerHub.js
@@ -5,15 +5,52 @@ import Tasks from "./Tasks";
 import Interests from "./Interests";
 import Explore from "./Explore/Explore";
 import {useMediaQuery} from "react-responsive";
+import {useEffect, useState} from "react";
+import {useNavigate} from "react-router-dom";
+import {toast, ToastContainer} from "react-toastify";
 
 const CareerHub = () => {
     const isMediumScreen = useMediaQuery({minWidth: 998}); // Set the breakpoint for md screens
+    const [authenticated, setAuthenticated] = useState(false)
+    const navigate = useNavigate()
 
+    useEffect(()=>{
+        const token = localStorage.getItem("token")
+        console.log('Here is the token', token)
+        const validateToken = async ()=>{
+           if(token){
+               const response = await fetch('http://localhost:8080/api/validate-token/', {
+                   method: 'GET',
+                   headers:{
+                       'Authorization' : `Bearer ${token}`
+                   }
+               })
+               console.log('Here is the response', response)
+               if(!response.ok){
+                   toast.error("Token has expired please login", {
+                       onClose: ()=>{
+                           navigate('/accounts/login')
+                       }
+                   })
+
+               }
+           }
+           else{
+               toast.error('There is no valid token, login to receive a token', {
+                   onClose: ()=>{
+                       navigate('/accounts/login')
+                   }
+               })
+           }
+        }
+        validateToken()
+    },[])
     return (
         <>
             <NavBar />
             <Ribbon/>
             <div className={`flex ${isMediumScreen ? "gap-x-6" : "flex-col gap-y-6"} justify-center mt-1`}>
+                <ToastContainer position="top-center" />
                 {isMediumScreen ? (
                     <>
                         <div className="flex flex-col">
