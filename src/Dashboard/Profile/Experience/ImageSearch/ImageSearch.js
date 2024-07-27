d2f15f143e856affd96a168c19f8e1cb28fd7479
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-07-27 10:59:47 -0500

    feat: added usage of token validation

--- src/Dashboard/Profile/Experience/ImageSearch/ImageSearch.js
+++ src/Dashboard/Profile/Experience/ImageSearch/ImageSearch.js
@@ -6,24 +6,37 @@ const ImageSearch = ({query, handleImageUrl, onError}) => {
         const signal = abortController.signal
         console.log('Here is the query', query)
         const fetchImage = async () => {
+            const token = localStorage.getItem("token")
             try {
-                const response = await fetch(`http://localhost:8080/api/fetch-image/imageUrl?query=${query}`, {signal})
-                if (!response.ok) {
-                    throw new Error();
-                }
-                const dataJson = await response.json()
+                if (token) {
+                    const response = await fetch(`http://localhost:8080/api/fetch-image/imageUrl?query=${query}`, {
+                        method: 'GET',
+                        headers:{
+                            'Authorization' : `Bearer ${token}`
+                        },
+                        signal
+                    })
 
-                const data = dataJson.data
-                if (data.items && data.items.length > 0) {
-                    handleImageUrl(data.items[7].link)
-                } else {
-                    console.log('No images found')
-                    onError('No images found')
+                    if (!response.ok) {
+                        throw new Error();
+                    }
+                    const dataJson = await response.json()
+
+                    const data = dataJson.data
+                    if (data.items && data.items.length > 0) {
+                        handleImageUrl(data.items[7].link)
+                    } else {
+                        console.log('No images found')
+                        onError('No images found')
+                    }
                 }
-            } catch (err) {
-                onError("Image couldn't be fetched")
-                //throw err
             }
+            catch
+                (err)
+                {
+                    onError("Image couldn't be fetched")
+                    //throw err
+                }
         }
         fetchImage()
         return () => {
