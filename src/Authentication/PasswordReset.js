9985e8eccc977394902e25aa459c90baeb62fef0
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-07-27 17:58:30 -0500

    feat: added fetching of users info and then appending the return object with the new users password

--- src/Authentication/PasswordReset.js
+++ src/Authentication/PasswordReset.js
@@ -2,12 +2,13 @@ import { MdSecurity } from "react-icons/md";
 import { TbPasswordUser } from "react-icons/tb";
 import {useDispatch, useSelector} from "react-redux";
 import {setLoading} from "../Redux/UserSlice";
-import {useState} from "react";
+import React, {useState} from "react";
 import {toast, ToastContainer} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css'
 import {SyncLoader} from "react-spinners";
 import {useNavigate} from "react-router-dom";
 import Back from "../Buttons/Back";
+import Submit from "../Buttons/Submit";
 const PasswordReset = ()=>{
     const usr = useSelector(state=> state.userInfo)
     const dispatch = useDispatch()
@@ -17,6 +18,7 @@ const PasswordReset = ()=>{
     const[pass1, setPass1] = useState("")
     const[pass2, setPass2] = useState("")
     const[disabled, setDisabled] = useState(false)
+    const[hovered, setHovered] = useState(false)
 
     //to be used to check if the password matches the requirement
     const[passRem, setPassRem] = useState(true)
@@ -44,11 +46,19 @@ const PasswordReset = ()=>{
         }
         dispatch(setLoading(true))
         const formData = new FormData(e.target)
+        const email = usr.usrEmail
+
+        const userInfo = await fetch(`http://localhost:8080/api/person/${email}/`, {
+            method: 'GET'
+        });
+
+        const user = await userInfo.json()
+        console.log()
         const requestBody = {
-            email: usr.usrEmail,
+            ...user,
             password: formData.get('pass1')
         }
-        console.log(usr.email)
+
         const response = await fetch(`http://localhost:8080/accounts/login/password-reset/`, {
             method: 'post',
             headers: {
@@ -62,22 +72,22 @@ const PasswordReset = ()=>{
             toast.success(data, {
                 onClose: ()=>{
 
-                    navigate("../accounts/login")
+                    navigate("/accounts/login")
                 }
             })
         }
         else {//500
             toast.error(data, {
                 onClose: ()=>{
-                    navigate("../accounts/signup")
+                    navigate("/accounts/signup")
                 }
             })
         }
 
     }
     return(
-        <div className="flex flex-col justify-center items-center h-screen ">
-            <div className="border rounded-lg w-[400px]">
+        <div className="flex flex-col justify-center items-center h-screen bg-[#f0f2f5]">
+            <div className="border rounded-lg w-[400px] bg-white">
                 <ToastContainer position={"top-center"} />
                 <div className="flex flex-col justify-center items-center mt-8 ">
                     <TbPasswordUser size={35} color="gray"/>
@@ -87,24 +97,26 @@ const PasswordReset = ()=>{
                 <form onSubmit={(event)=>handleSubmit(event)}>
                 <div className="flex flex-col">
                     <input value={pass1} disabled={disabled} onChange={(e)=> setPass1(e.target.value)}
-                           className="my-3 border rounded-md p-2 w-[90%] mx-4 outline-none focus:border-gray-500" type="password" name="pass1"
+                           className="my-3 border border-gray-400 rounded-md p-2 w-[90%] mx-4 outline-none focus:border-[#367c2b]" type="password" name="pass1"
                            placeholder="New password"/>
                     <input value={pass2} disabled={disabled} onChange={(e)=> setPass2(e.target.value)}
-                           className="my-3 border rounded-md p-2 w-[90%] mx-4 outline-none focus:border-gray-500" type="password" name="pass2"
+                           className="my-3 border border-gray-400 rounded-md p-2 w-[90%] mx-4 outline-none focus:border-[#367c2b]" type="password" name="pass2"
                            placeholder="Confirm new password"/>
                 </div>
                 {!passMatch && <p className="bg-[#ffebe8] p-2 mb-3 ml-4 rounded-md 2 w-[90%] font-medium">Password don't match</p>}
                 {!passRem && <p className="bg-[#ffebe8] p-1 mb-3 ml-4 rounded-md 2 w-[90%] font-medium"> At least 16 characters OR at least 8 characters including a number and a letter.</p>}
-                <div className="">
-                    <button disabled={disabled} className="bg-blue-600 rounded-md p-2 w-[90%] my-3 mr-2 ml-5 text-white" type='submit'>{usr.loading ? <SyncLoader size={12} color="blue" loading={ usr.loading}/>: 'Set Password'}</button>
-                </div>
+                    <button disabled={disabled}
+                            className="w-[86%] hover:bg-[#367c2b] border border-[#367c2b] rounded-lg my-4 ml-7 p-2 text-white flex justify-center"
+                            onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
+                        <Submit text={"Reset Password"} disabled={disabled} hovered={hovered}/>
+                    </button>
                 </form>
                 <div className="flex justify-center my-6">
                     <Back text={"Found you Password"} margin={"12"} disabled={disabled}/>
                 </div>
                 <div className="flex justify-center my-6">
                     <MdSecurity size={23} className="mr-2"/>
-                    <p>Secured by Task Ninja</p>
+                    <p>Secured by Job Bazaar</p>
                 </div>
             </div>
         </div>
