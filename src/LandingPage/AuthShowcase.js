2d2a210e1063d3b997f56ea683205171d2d9d989
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-07-27 18:11:45 -0500

    refractor: removed unused variables and imports

--- src/LandingPage/AuthShowcase.js
+++ src/LandingPage/AuthShowcase.js
@@ -1,22 +1,22 @@
 import ImageLeft from "../Images/b.svg";
 import ImageRight from "../Images/c.svg";
 import LoginPanel from "./LoginPanel";
-import {useDispatch, useSelector} from "react-redux";
+import {useSelector} from "react-redux";
 import PanelShowcase from "./PanelShowcase";
 
-const AuthShowcase = ()=>{
+const AuthShowcase = () => {
     const usr = useSelector(state => state.userInfo)
-    const dispatch = useDispatch()
 
-    return(
+    return (
         <>
-            <h1 className="text-center md:text-5xl text-4xl text-[#00A264] font-medium pt-10">Your work people are here</h1>
+            <h1 className="text-center md:text-5xl text-4xl text-[#367c2b] font-medium pt-10">Your work people are
+                here</h1>
             <div className="flex lg:flex-row flex-col items-center xl:justify-evenly">
                 <div className="lg:ml-28">
                     <img src={ImageLeft} alt="" className="h-[450px] w-[500px]"/>
                 </div>
-                {usr.firstPanel && <LoginPanel />}
-                {usr.secondPanel && <PanelShowcase />}
+                {usr.firstPanel && <LoginPanel/>}
+                {usr.secondPanel && <PanelShowcase/>}
 
                 <div className="lg:ml-24 ml-24">
                     <img src={ImageRight} alt="" className="h-[500px] w-[500px]"/>
