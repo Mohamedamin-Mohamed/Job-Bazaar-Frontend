38a2b0f2a16da10619b350e697af1affbf4023f4
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-07-27 10:57:00 -0500

    feat: added usage of token validation

--- src/Dashboard/Profile/Experience/Education/FetchEducation/SaveEducation.js
+++ src/Dashboard/Profile/Experience/Education/FetchEducation/SaveEducation.js
@@ -1,9 +1,10 @@
 const SaveEducation = async (postRequest) => {
-
+    const token = localStorage.getItem("token")
     return await fetch('http://localhost:8080/api/user-education/save', {
         method: 'post',
         headers: {
             'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
         },
         body: JSON.stringify(postRequest),
     })
--- src/Dashboard/Profile/Experience/Education/FetchEducation/UpdateEducation.js
+++ src/Dashboard/Profile/Experience/Education/FetchEducation/UpdateEducation.js
@@ -1,8 +1,10 @@
 const UpdateEducation = async (updateRequest)=>{
+    const token = localStorage.getItem("token")
     return await fetch('http://localhost:8080/api/user-education/update', {
         method: 'PATCH',
         headers:{
-            'Content-type' : 'application/json'
+            'Content-Type' : 'application/json',
+            'Authorization' : `Bearer ${token}`
         },
         body: JSON.stringify(updateRequest)
     })
