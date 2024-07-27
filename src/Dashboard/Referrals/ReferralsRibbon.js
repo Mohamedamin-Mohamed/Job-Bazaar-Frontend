1d3e3f8106f7f3409b5359d4e7a1d9b3cc4fde87
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-07-27 18:09:34 -0500

    fix: changed height of the outermost div to use the hard coded height instead of using the argument being passed to it

--- src/Dashboard/Referrals/ReferralsRibbon.js
+++ src/Dashboard/Referrals/ReferralsRibbon.js
@@ -1,10 +1,11 @@
 import NavBar from "../Careerhub/NavBar";
-const ReferralsRibbon = ({ text, height })=>{
-    return(
+
+const ReferralsRibbon = ({text}) => {
+    return (
         <>
             <div>
-                <NavBar />
-                <div className={`relative mx-10 h-${height} flex overflow-hidden mt-0.5`}>
+                <NavBar/>
+                <div className={`relative mx-10 h-[200px] flex overflow-hidden mt-0.5`}>
                     <div className="absolute inset-0 flex">
                         <div className="w-1/2 h-full bg-[#ffde00]"
                              style={{clipPath: 'polygon(0 0, 100% 0, 0 100%'}}></div>
@@ -12,7 +13,7 @@ const ReferralsRibbon = ({ text, height })=>{
                              style={{clipPath: 'polygon(0 0, 100% 0, 100% 100%'}}></div>
                     </div>
                     <div className="ml-6 mt-14 z-50">
-                        <h1 className="text-3xl font-mono font-bold">{ text }</h1>
+                        <h1 className="text-3xl font-mono font-bold">{text}</h1>
                     </div>
                 </div>
             </div>
