20cec1f51e808332405204387bfc928003c57cc3
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-07-27 18:07:16 -0500

    refractor: removed unused imports and variables

--- src/Dashboard/Careerhub/CareerHub.js
+++ src/Dashboard/Careerhub/CareerHub.js
@@ -5,52 +5,50 @@ import Tasks from "./Tasks";
 import Interests from "./Interests";
 import Explore from "./Explore/Explore";
 import {useMediaQuery} from "react-responsive";
-import {useEffect, useState} from "react";
+import {useEffect} from "react";
 import {useNavigate} from "react-router-dom";
 import {toast, ToastContainer} from "react-toastify";
 
 const CareerHub = () => {
     const isMediumScreen = useMediaQuery({minWidth: 998}); // Set the breakpoint for md screens
-    const [authenticated, setAuthenticated] = useState(false)
     const navigate = useNavigate()
 
-    useEffect(()=>{
+    useEffect(() => {
         const token = localStorage.getItem("token")
-        console.log('Here is the token', token)
-        const validateToken = async ()=>{
-           if(token){
-               const response = await fetch('http://localhost:8080/api/validate-token/', {
-                   method: 'GET',
-                   headers:{
-                       'Authorization' : `Bearer ${token}`
-                   }
-               })
-               console.log('Here is the response', response)
-               if(!response.ok){
-                   toast.error("Token has expired please login", {
-                       onClose: ()=>{
-                           navigate('/accounts/login')
-                       }
-                   })
 
-               }
-           }
-           else{
-               toast.error('There is no valid token, login to receive a token', {
-                   onClose: ()=>{
-                       navigate('/accounts/login')
-                   }
-               })
-           }
+        const validateToken = async () => {
+            if (token) {
+                const response = await fetch(`http://localhost:8080/api/validate-token/`, {
+                    method: 'GET',
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    }
+                })
+
+                if (!response.ok) {
+                    toast.error("Token has expired please login", {
+                        onClose: () => {
+                            navigate('/accounts/login')
+                        }
+                    })
+
+                }
+            } else {
+                toast.error('There is no valid token, login to receive a token', {
+                    onClose: () => {
+                        navigate('/accounts/login')
+                    }
+                })
+            }
         }
         validateToken()
-    },[])
+    }, [])
     return (
         <>
-            <NavBar />
+            <NavBar/>
             <Ribbon/>
             <div className={`flex ${isMediumScreen ? "gap-x-6" : "flex-col gap-y-6"} justify-center mt-1`}>
-                <ToastContainer position="top-center" />
+                <ToastContainer position="top-center"/>
                 {isMediumScreen ? (
                     <>
                         <div className="flex flex-col">
@@ -59,7 +57,7 @@ const CareerHub = () => {
                         </div>
                         <div className="flex flex-col">
                             <Interests/>
-                                <Explore/>
+                            <Explore/>
                         </div>
                     </>
                 ) : (
@@ -68,7 +66,7 @@ const CareerHub = () => {
                             <div className="mx-4">
                                 <Interests/>
                             </div>
-                                <Explore />
+                            <Explore/>
 
                         </div>
                         <div className="flex flex-col justify-center items-center z-50">
