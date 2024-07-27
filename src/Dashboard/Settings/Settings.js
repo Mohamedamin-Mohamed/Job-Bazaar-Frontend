ffebccc69d5fab596dc03e19961250e58a90ed87
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-07-27 18:10:56 -0500

    refractor: removed unused variables and imports

--- src/Dashboard/Settings/Settings.js
+++ src/Dashboard/Settings/Settings.js
@@ -1,15 +1,12 @@
 import ReferralsRibbon from "../Referrals/ReferralsRibbon";
-import {BiSolidUserAccount} from "react-icons/bi";
-import {useState} from "react";
 import Account from "./Account";
 import Activity from "./Activity";
 import JobAlerts from "./JobAlerts";
 
 const Settings = () => {
-    const [termsAndServices, setTermsAndServices] = useState(false)
     return (
         <>
-            <ReferralsRibbon text={"Settings"} height={60}/>
+            <ReferralsRibbon text={"Settings"}/>
             <Account/>
             <Activity/>
             <JobAlerts/>
