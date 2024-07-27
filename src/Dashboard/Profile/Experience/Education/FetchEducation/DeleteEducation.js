3c5afb9d9277062dc6e3a6b40cb24ee5ad3ba280
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-07-27 10:55:58 -0500

    feat: added usage of token validation

--- src/Dashboard/Profile/Experience/Education/FetchEducation/DeleteEducation.js
+++ src/Dashboard/Profile/Experience/Education/FetchEducation/DeleteEducation.js
@@ -1,6 +1,10 @@
 const DeleteEducation = async (email)=>{
+    const token = localStorage.getItem("token")
     return await fetch(`http://localhost:8080/api/user-education/delete/${email}`, {
         method: "DELETE",
+        headers:{
+            'Authorization' : `Bearer ${token}`
+        }
     });
 
 }
