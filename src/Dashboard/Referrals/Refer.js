3c99a78361bcc41d686e769601f1eea89bcd035d
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-08-02 18:59:51 -0500

    feat: added rbac to the navigation

--- src/Dashboard/Referrals/Refer.js
+++ src/Dashboard/Referrals/Refer.js
@@ -1,27 +1,30 @@
-import NavBar from "../Careerhub/NavBar";
 import Ribbon from "../Careerhub/GenericRibbon";
-import {FaCloudArrowUp} from "react-icons/fa6";
 import {IoMdShareAlt} from "react-icons/io";
 import Image from "../../Images/refer_illustration_cloud.png"
-import {useRef, useState} from "react";
+import {useState} from "react";
 import ReferralsLinks from "./ReferralsLinks";
 import {useNavigate} from "react-router-dom";
 
 const Refer = () => {
-    const[linksShow, setLinksShow] = useState(false)
+    const [linksShow, setLinksShow] = useState(false)
     const navigate = useNavigate()
-    const ref = useRef()
-    const handleLinksShow = ()=>{
+    const user = JSON.parse(localStorage.getItem('user'))
+    const role = user.role
+    const handleLinksShow = () => {
         setLinksShow(!linksShow)
     }
-    const handleClick = ()=>{
-        navigate('/careerhub/explore/jobs')
+    const handleClick = () => {
+        if (role === 'Employer')
+            navigate('/careerhub/my/jobs/uploaded')
+        else
+            navigate('/careerhub/explore/jobs')
     }
     return (
         <>
             <Ribbon text={"Refer a Friend"}/>
             <div className="flex items-center flex-col h-screen mt-4">
-                <div className="flex flex-col items-center bg-[#2c8cc90a] border border-dashed border-black w-[300px] h-[470px]">
+                <div
+                    className="flex flex-col items-center bg-[#2c8cc90a] border border-dashed border-black w-[300px] h-[470px]">
                     <img src={Image} alt="" className="w-[72px] h-[52px] mt-2 mb-8"/>
                     <h1 className="w-[78%] text-center text-[#367c2b] text-2xl font-semibold">Drop your friend's resume
                         here.</h1>
@@ -42,15 +45,19 @@ const Refer = () => {
                         <h1 className="text-[#4f5666]">If you are an employee from Europe or Canada</h1>
                         <div className="flex mt-2">
                             <h1 className="text-[#367c2b] font-semibold text-2xl">Share link with your friends</h1>
-                            <IoMdShareAlt color="gray" className="bg-white rounded ml-3 p-1 w-[44px] h-[36px] hover:cursor-pointer" onClick={handleLinksShow}/>
+                            <IoMdShareAlt color="gray"
+                                          className="bg-white rounded ml-3 p-1 w-[44px] h-[36px] hover:cursor-pointer"
+                                          onClick={handleLinksShow}/>
                         </div>
-                        {linksShow && <ReferralsLinks handleLinksShow={handleLinksShow} />}
+                        {linksShow && <ReferralsLinks handleLinksShow={handleLinksShow}/>}
                     </div>
                 </div>
                 <div className="flex flex-col justify-center items-center mt-4">
                     <p>- Or -</p>
-                    <button className="flex justify-center text-[#367c2b] font-medium mr-12 w-48 mb-8 bg-white border border-[#367c2b] ml-16 mt-4 pl-3 py-1 hover:bg-[#367c2b] hover:text-white" onClick={ handleClick }>Skip
-                        And Browse Jobs
+                    <button
+                        className={`flex justify-center text-[#367c2b] font-medium mr-12 ${role === 'Employer' ? 'w-64' : 'w-48'} w-48 mb-8 bg-white border border-[#367c2b] ml-16 mt-4 pl-3 py-1 hover:bg-[#367c2b] hover:text-white`}
+                        onClick={handleClick}>
+                        {`${role === 'Employer' ? 'Skip and Browse Uploaded Jobs' : 'Skip and Browse Jobs'}`}
                     </button>
                 </div>
             </div>
