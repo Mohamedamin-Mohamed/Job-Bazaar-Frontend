876ab5584a8970d55457d5457dea26447de496c3
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-08-04 22:24:11 -0500

    fix: fixed the path for requesting available jobs

---
+++ src/Dashboard/Careerhub/Jobs/FetchJobs/GetAvailableJobs.js
@@ -0,0 +1,12 @@
+const GetAvailableJobs = async (controller)=>{
+    const token = localStorage.getItem('token')
+
+    return await fetch('http://localhost:8080/api/jobs/', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        },
+        signal: controller.signal
+    })
+}
+export default GetAvailableJobs
