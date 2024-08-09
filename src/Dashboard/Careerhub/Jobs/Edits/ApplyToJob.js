cfbfd71c248a9487da52967c20a6c1513bfbfefa
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-08-09 12:03:50 -0500

    feat: new component that builds a formData with info of the users application and sends it to the backend to be saved

---
+++ src/Dashboard/Careerhub/Jobs/Edits/ApplyToJob.js
@@ -0,0 +1,36 @@
+import {format} from "date-fns";
+
+const ApplyToJob = async (controller, jobApplication, user) => {
+    const token = localStorage.getItem('token')
+    const formData = new FormData();
+    formData.append("applicantEmail", user.email);
+    formData.append("jobId", jobApplication.jobId);
+    formData.append("resumeName", jobApplication.resume.name)
+    formData.append("resumeFile", jobApplication.resume.file)
+    formData.append("country", jobApplication.country)
+    formData.append("city", jobApplication.city)
+    formData.append("postalCode", jobApplication.postalCode)
+    formData.append("gender", jobApplication.gender)
+    formData.append("nationality", jobApplication.nationality)
+    formData.append("additionalDocName", jobApplication.additionalDocument.name)
+    formData.append("additionalDocFile", jobApplication.additionalDocument.file)
+    formData.append("employerContact", jobApplication.employerContact)
+    formData.append("firstName", user.firstName)
+    formData.append("lastName", user.lastName)
+
+    const date = new Date().toISOString()
+    const formattedDate = date ? format(date, 'MM-dd-yyy') : ''
+   formData.append("applicationDate", formattedDate)
+
+    if (token) {
+        return await fetch('http://localhost:8080/api/applications/add', {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${token}`
+            },
+            body: formData,
+            signal: controller.signal
+        })
+    }
+}
+export default ApplyToJob
