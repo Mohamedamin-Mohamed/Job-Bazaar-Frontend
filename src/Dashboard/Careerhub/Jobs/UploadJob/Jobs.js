726bc240eb01a8800cc813ded3983ab093f7d4d5
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-08-14 22:46:02 -0500

    feat: added role based check and perform tasks designated for each user

--- src/Dashboard/Careerhub/Jobs/UploadJob/Jobs.js
+++ src/Dashboard/Careerhub/Jobs/UploadJob/Jobs.js
@@ -1,11 +1,21 @@
 import NavBar from "../../NavBar";
 import UploadJobs from "./UploadJobs";
+import Display404EmployerOrApplicant from "../DisplayJobsAppliedTo/Display404EmployerOrApplicant";
 
 const Jobs = () => {
+    const userInfo = JSON.parse(localStorage.getItem('user'))
+    const role = userInfo.role
+
     return (
         <>
-            <NavBar/>
-            <UploadJobs/>
+            {role === 'Applicant' ?
+                <Display404EmployerOrApplicant role={role}/>
+                :
+                <>
+                    <NavBar/>
+                    <UploadJobs/>
+                </>
+            }
         </>
     )
 }
