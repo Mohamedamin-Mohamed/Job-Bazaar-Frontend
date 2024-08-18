52a86e356ee8b08d6f41984f078e4bc68c8e4b9e
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-08-17 21:06:42 -0500

    feat: added new destructured prop and counting days logic

--- src/Dashboard/Careerhub/Jobs/DisplayJobsAppliedTo/Job Description/Header.js
+++ src/Dashboard/Careerhub/Jobs/DisplayJobsAppliedTo/Job Description/Header.js
@@ -2,8 +2,18 @@ import {SlLocationPin} from "react-icons/sl";
 import {GiSuitcase} from "react-icons/gi";
 import {HiOutlineClock} from "react-icons/hi2";
 import {FaIdBadge} from "react-icons/fa";
+import {format} from "date-fns";
 
-const Header = ({ job })=>{
+const Header = ({job, applicationDate}) => {
+    const date = new Date().toISOString()
+    const formattedDate = format(date, 'MM-dd-yyy')
+
+    const appliedDate = new Date(applicationDate)
+    const currDate = new Date(formattedDate)
+
+    const timeDifference = currDate - appliedDate
+    const daysDifference = timeDifference / (1000 * 60 * 60 * 24)
+
     return (
         <div className="flex md:flex-row md:space-x-16 flex-col mt-4">
             <div className="flex space-x-2 p-2">
@@ -17,7 +27,7 @@ const Header = ({ job })=>{
                 </div>
                 <div className="flex space-x-2 p-2">
                     <HiOutlineClock size={22} color="gray"/>
-                    <p>{job.postedDate}</p>
+                    <p>Posted {daysDifference > 0 ? (daysDifference + ` day${daysDifference !== 1 ? "'s" : ""} ago`) : "less than a day ago"}</p>
                 </div>
                 <div className="flex space-x-2 p-2">
                     <FaIdBadge size={22} color="gray"/>
