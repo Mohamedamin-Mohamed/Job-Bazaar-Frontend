f40211c1520f5e88e263a746123cfd0176704f1f
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-08-17 21:08:29 -0500

    feat: addded centering of the child components

--- src/Dashboard/Careerhub/Jobs/DisplayJobsAppliedTo/Job Description/ApplicationDescription.js
+++ src/Dashboard/Careerhub/Jobs/DisplayJobsAppliedTo/Job Description/ApplicationDescription.js
@@ -1,15 +1,15 @@
-import CompanyInfo from "../CompanyInfo";
+import CompanyInfo from "./CompanyInfo";
 import Description from "./Description";
 import {useMediaQuery} from "react-responsive";
 import NavBar from "../../../NavBar";
 
 const ApplicationDescription = () => {
-    const mediaQuery = useMediaQuery({minWidth: "1440px"})
+    const mediaQuery = useMediaQuery({minWidth: "1284px"})
     return (
-        <div className=" pb-6">
+        <div>
             <NavBar/>
-            <div className={`${mediaQuery ? "flex" : "flex-col"} bg-[#f0f1f2]`}>
-                <div className="flex flex-col">
+            <div className={`${mediaQuery ? "flex" : "flex-col"} py-6 justify-center bg-[#f0f1f2]`}>
+                <div className="flex flex-col justify-center items-center">
                     <Description/>
                 </div>
                 <CompanyInfo/>
