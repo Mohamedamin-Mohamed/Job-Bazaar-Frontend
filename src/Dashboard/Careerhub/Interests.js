94ddccde51b119e1268abde1c32b190b91bc0b43
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-08-18 21:09:19 -0500

    refractor: removed unused imports

--- src/Dashboard/Careerhub/Interests.js
+++ src/Dashboard/Careerhub/Interests.js
@@ -1,18 +1,21 @@
 import {useNavigate} from "react-router-dom";
 
-const Interests = ()=>{
+const Interests = () => {
     const navigate = useNavigate()
-    const handleNavigation = ()=>{
+    const handleNavigation = () => {
         navigate('/careerhub/profile/career')
     }
-    return(
-        <div className="flex md:flex-row flex-col justify-self-center items-center h-[200px] bg-[#f7f7f7] mt-10">
+    return (
+        <div className="flex md:flex-row flex-col items-center h-[200px] bg-[#f7f7f7] mt-10">
             <div className="flex flex-col p-6">
                 <h1 className="mb-4 font-medium md:text-xl">Take 15 mins to set your career interests</h1>
-                <p className="md:w-[80%] text-sm md:text-lg">Tell us what you want to achieve in your career, and we'll put a plan together for you.</p>
+                <p className="md:w-[80%] text-sm md:text-lg">Tell us what you want to achieve in your career, and we'll
+                    put a plan together for you.</p>
             </div>
             <div className="flex justify-center items-center">
-                <button className="bg-[#ffde00] text-black w-[206px] h-[36px] font-medium mr-4" onClick={handleNavigation}>Set Your Career Interests</button>
+                <button className="bg-[#ffde00] text-black w-[206px] h-[36px] font-medium mr-4"
+                        onClick={handleNavigation}>Set Your Career Interests
+                </button>
             </div>
         </div>
     )
