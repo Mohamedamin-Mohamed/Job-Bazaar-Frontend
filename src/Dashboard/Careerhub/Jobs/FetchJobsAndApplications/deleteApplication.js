748d0cb969b70a14be852225d74e98278bd56169
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-08-19 21:52:42 -0500

    refractor: removed unneeded log

--- src/Dashboard/Careerhub/Jobs/FetchJobsAndApplications/deleteApplication.js
+++ src/Dashboard/Careerhub/Jobs/FetchJobsAndApplications/deleteApplication.js
@@ -1,5 +1,4 @@
 const deleteApplication = async (applicantEmail, jobId, controller) => {
-    console.log('applicant email is', applicantEmail)
     const token = localStorage.getItem('token')
     if (token) {
         return await fetch(`http://localhost:8080/api/applications/delete/${applicantEmail}/${jobId}`, {
