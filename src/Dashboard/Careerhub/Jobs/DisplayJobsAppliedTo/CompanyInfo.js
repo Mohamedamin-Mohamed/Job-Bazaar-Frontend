1955cb421aab02a78c543502d3cd0e59ed5d2a50
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-08-19 21:49:41 -0500

    style: adjusted margin top of outer div

--- src/Dashboard/Careerhub/Jobs/DisplayJobsAppliedTo/CompanyInfo.js
+++ src/Dashboard/Careerhub/Jobs/DisplayJobsAppliedTo/CompanyInfo.js
@@ -1,26 +1,11 @@
 import {useMediaQuery} from "react-responsive";
-import {useEffect, useRef, useState} from "react";
 
 const CompanyInfo = () => {
     const mediaQuery = useMediaQuery({minWidth: "1464px"})
-    const refHeight = useRef(null)
-    const [height, setHeight] = useState("")
+    const mediaQuery2 = useMediaQuery({minWidth: "768px"})
 
-    useEffect(() => {
-        const updateHeight = () => {
-            if (refHeight.current) {
-                setHeight(refHeight.current.offsetHeight)
-            }
-        }
-        updateHeight() //initial render
-        window.addEventListener('resize', updateHeight)
-        return () => {
-            window.removeEventListener('resize', updateHeight)
-        }
-    }, [])
     return (
-        //  style={{height: `${height}px`}} ref={refHeight}
-        <div className={`flex flex-col ${mediaQuery ? "mx-6" : "mx-10"} bg-white p-6 border rounded-xl mb-8`}>
+        <div className={`flex flex-col ${mediaQuery ? "mx-6 mt-20 h-[580px]" : "mx-10 mt-6"} ${!mediaQuery2 ? "mt-8" : ""} bg-white p-6 border rounded-xl mb-8`}>
             <div className={`${!mediaQuery ? `` : "w-[300px]"}`}>
                 <h1 className="text-xl font-semibold my-3">Welcome</h1>
                 <p className="text-[#494949]">Thank you for using Job Bazaar</p>
@@ -31,7 +16,8 @@ const CompanyInfo = () => {
                 <p className="text-[#494949]">Job Bazaar is a streamlined platform that connects employers with
                     top talent. Employers can efficiently post jobs, manage applications, and find the ideal candidates,
                     while job seekers can explore opportunities and track their application progress. Job Bazaar
-                    simplifies recruitment for both employers and job seekers, making the process efficient and user-friendly..</p>
+                    simplifies recruitment for both employers and job seekers, making the process efficient and
+                    user-friendly..</p>
             </div>
         </div>
     )
