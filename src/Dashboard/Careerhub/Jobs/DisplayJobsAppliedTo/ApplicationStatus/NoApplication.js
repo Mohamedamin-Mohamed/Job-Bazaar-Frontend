24798ce1faf475b3d822f1c44f20c732a50fe85e
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-08-19 21:51:19 -0500

    feat: added new buttons to redirects users with role applicant to search for jobs

--- src/Dashboard/Careerhub/Jobs/DisplayJobsAppliedTo/ApplicationStatus/NoApplication.js
+++ src/Dashboard/Careerhub/Jobs/DisplayJobsAppliedTo/ApplicationStatus/NoApplication.js
@@ -1,10 +1,13 @@
 import Image from "../../../../../Images/no_application_banner.svg"
+import {NavLink} from "react-router-dom";
 
-const NoApplication = ()=>{
-    return(
+const NoApplication = () => {
+    return (
         <div className="flex flex-col justify-center items-center w-full h-[348px]">
-            <img src={Image} alt="" />
+            <img src={Image} alt=""/>
             <p>You have no applications</p>
+            <NavLink to="/careerhub/explore/jobs" className=" flex justify-center items-center border border-[#367c2b] text-[#367c2b] hover:bg-[#367c2b]
+            hover:text-white w-[180px] h-[36px] mt-4 rounded-sm">Search for Jobs</NavLink>
         </div>
     )
 }
