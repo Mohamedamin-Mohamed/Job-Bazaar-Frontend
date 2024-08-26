ca6348994e43d0d804b61c491fa46f15f6cb53d8
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-08-25 19:47:07 -0500

    feat: added new function that updates application status

---
+++ src/Dashboard/Careerhub/Jobs/FetchJobsAndApplications/updateApplication.js
@@ -0,0 +1,14 @@
+const updateApplication = async (applicantEmail, jobId, applicationStatus, controller) => {
+    const token = localStorage.getItem('token')
+    if (token) {
+        return await fetch(`http://localhost:8080/api/applications/updateApplicationStatus/${applicantEmail}/${jobId}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+            body: JSON.stringify({applicationStatus: applicationStatus})
+        })
+    }
+}
+export default updateApplication
