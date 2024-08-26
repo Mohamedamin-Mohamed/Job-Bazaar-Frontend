4bfa853f2a14e7bce70980f96020a286a7e346be
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-08-25 19:48:07 -0500

    feat: new function that updates a jobs status

--- src/Dashboard/Careerhub/Jobs/FetchJobsAndApplications/updateJob.js
+++ src/Dashboard/Careerhub/Jobs/FetchJobsAndApplications/updateJob.js
@@ -1,14 +1,16 @@
-const deleteJob = async (employerEmail, jobId, controller) => {
+const updateJob = async (employerEmail, jobId, jobStatus, controller) => {
     const token = localStorage.getItem('token')
     if (token) {
-        return await fetch(`http://localhost:8080/api/jobs/delete/${employerEmail}/${jobId}`, {
-                method: 'DELETE',
+        return await fetch(`http://localhost:8080/api/jobs/update/${employerEmail}/${jobId}`, {
+                method: 'PATCH',
                 headers: {
+                    'Content-type': 'application/json',
                     'Authorization': `Bearer ${token}`
                 },
+                body: JSON.stringify({jobStatus: jobStatus}),
                 signal: controller.signal
             }
         )
     }
 }
-export default deleteJob
+export default updateJob
