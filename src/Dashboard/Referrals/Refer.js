5a4d0d248496b492018c0b64477a52c9571973f3
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-09-04 19:58:06 -0500

    feat: added an input and label tag to let users select file to be used for a referral

--- src/Dashboard/Referrals/Refer.js
+++ src/Dashboard/Referrals/Refer.js
@@ -4,6 +4,9 @@ import Image from "../../Images/refer_illustration_cloud.png"
 import {useState} from "react";
 import ReferralsLinks from "./ReferralsLinks";
 import {useNavigate} from "react-router-dom";
+import {format} from "date-fns";
+import addReferral from "../Careerhub/Jobs/FetchJobsAndApplications/addReferral";
+import {toast, ToastContainer} from "react-toastify";
 
 const Refer = () => {
     const [linksShow, setLinksShow] = useState(false)
@@ -19,10 +22,40 @@ const Refer = () => {
         else
             navigate('/careerhub/explore/jobs')
     }
+    const handleReferral = async (event) => {
+        const file = event.target.files[0]
+        const formData = new FormData()
+        const fullName = user.firstName + " " + user.lastName
+        const date = new Date()
+        const formattedDate = format(date, 'MM-yyyy-dd')
+
+        formData.append("referrerEmail", user.email)
+        formData.append("referrerName", fullName)
+        formData.append("fileName", file.name)
+        formData.append("createdAt", formattedDate)
+        formData.append("refereeResumeFile", file)
+        try {
+            const response = await addReferral(formData, new AbortController())
+            if (!response.ok) {
+                throw new Error("Couldn't add referral")
+            } else {
+                const text = await response.text()
+                toast.success(text, {
+                    onClose: () => {
+                        navigate('/careerhub/myreferrals')
+                    }
+                })
+            }
+        } catch (err) {
+            console.error(err)
+        }
+
+    }
     return (
         <>
             <Ribbon text={"Refer a Friend"}/>
             <div className="flex items-center flex-col h-screen mt-4">
+                <ToastContainer position="top-center"/>
                 <div
                     className="flex flex-col items-center bg-[#2c8cc90a] border border-dashed border-black w-[300px] h-[470px]">
                     <img src={Image} alt="" className="w-[72px] h-[52px] mt-2 mb-8"/>
@@ -33,11 +66,11 @@ const Refer = () => {
                         Due to data protection requirements, it is therefore not allowed to upload the resume.</p>
                     <p className="mb-1 mt-1">(doc, docx, pdf or txt)</p>
                     <p>or</p>
-
-                    <button
-                        className="flex justify-center text-[#367c2b] font-medium mr-12 w-36 mb-8 bg-white border border-[#367c2b] ml-16 mt-4 pl-3 py-1 hover:bg-[#367c2b] hover:text-white">BROWSE
+                    <input type="file" id="refer" className="hidden" onChange={(event) => handleReferral(event)}/>
+                    <label htmlFor="refer"
+                           className="flex justify-center text-[#367c2b] font-medium mr-12 w-36 mb-8 bg-white border border-[#367c2b] ml-16 mt-4 pl-3 py-1 hover:bg-[#367c2b] hover:text-white cursor-pointer">BROWSE
                         FILES
-                    </button>
+                    </label>
                 </div>
                 <div
                     className="flex flex-col justify-center items-center mt-6 w-[440px] h-[100px] bg-[#2c8cc90a] border border-dashed border-black rounded-2xl ">
