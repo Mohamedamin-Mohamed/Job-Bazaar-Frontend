fafcf76aeee9dbaeed901aa453e1a3faa2de490c
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-09-04 19:53:20 -0500

    feat: added display of number of referrals for users with role Applicant

--- src/Dashboard/Careerhub/Activity.js
+++ src/Dashboard/Careerhub/Activity.js
@@ -1,15 +1,17 @@
-import {NavLink} from "react-router-dom";
+import {NavLink, useNavigate} from "react-router-dom";
 import {useMediaQuery} from "react-responsive";
 import {useEffect, useState} from "react";
 import getAppliedJobs from "./Jobs/FetchJobsAndApplications/getAppliedJobs";
 import getUploadedJobs from "./Jobs/FetchJobsAndApplications/getUploadedJobs";
 import countActiveApplications from "./Jobs/DisplayJobsAppliedTo/CountApplications/countActiveApplications";
 import countActiveJobs from "./Management/CountJobs/countActiveJobs";
+import getReferrals from "./Jobs/FetchJobsAndApplications/getReferrals";
 
 const Activity = () => {
-    const isMediumScreen = useMediaQuery({minWidth: 998});// Set the breakpoint for md screens
-
+    const isMediumScreen = useMediaQuery({minWidth: 1370});// Set the breakpoint for md screens
+    const [referrals, setReferrals] = useState([])
     const userInfo = JSON.parse(localStorage.getItem('user'))
+    const navigate = useNavigate()
     const role = userInfo.role
     const email = userInfo.email
     const [jobs, setJobs] = useState([])
@@ -42,26 +44,41 @@ const Activity = () => {
         setCount(count)
     }, [jobs]);
 
+    useEffect(() => {
+        const fetchReferrals = async () => {
+            const response = await getReferrals(userInfo.email, new AbortController())
+            if (response.ok) {
+                const data = await response.json()
+                setReferrals(data)
+            }
+        }
+        fetchReferrals().catch(err => console.error(err))
+    }, []);
+
     return (
         <div
-            className={`"flex flex-col text-[#367c2b] mt-10 border ${!isMediumScreen ? "w-[88%] mx-12 p-4 mb-8" : "flex w-[345px] p-4"} h-[280px]"`}>
+            className={`"flex flex-col text-[#367c2b] mt-10 border ${!isMediumScreen ? "w-[93%] mx-12 p-4 relative" : "flex px-4 pt-3.5 mx-4"} h-[280px]"`}>
             <h1 className="text-black font-bold p-1.5">My activity</h1>
             <div className="flex justify-between p-2 font-bold hover:underline hover:cursor-pointer">
                 <NavLink
                     to={role === 'Employer' ? "my/jobs/uploaded" : "my/jobs/applied"}>{role === 'Employer' ? "Job Uploaded" : "Job Applications"}</NavLink>
                 <p className="bg-[#cfd9cc] rounded-md px-1">{count}</p>
             </div>
-            <div className="flex justify-between p-2 font-bold hover:underline hover:cursor-pointer">
-                <p>Project Applications</p>
-                <p className="bg-[#cfd9cc] rounded-md px-1">0</p>
-            </div>
-            <div className="flex justify-between p-2 border-b mb-4 pb-6 font-bold hover:underline hover:cursor-pointer">
-                <p>Referrals</p>
-                <p className="bg-[#cfd9cc] rounded-md px-1">0</p>
-            </div>
-            <div className="flex justify-center font-bold hover:underline hover:cursor-pointer">
-                <NavLink to='/refer'>Refer a friend</NavLink>
-            </div>
+            {role === 'Applicant' &&
+                (
+                    <>
+                        <div
+                            className="flex justify-between p-2 border-b mb-4 pb-6 font-bold hover:underline hover:cursor-pointer"
+                            onClick={() => navigate('myreferrals')}>
+                            <p>Referrals</p>
+                            <p className="bg-[#cfd9cc] rounded-md px-1">{referrals.length}</p>
+                        </div>
+
+                        <div className={`flex justify-center font-bold hover:underline hover:cursor-pointer`}>
+                            <NavLink to='/refer'>Refer a friend</NavLink>
+                        </div>
+                    </>
+                )}
         </div>
     )
 }
