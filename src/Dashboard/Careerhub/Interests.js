cd692d29e72263afdf7855d07328b1f620e14158
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-09-04 19:52:53 -0500

    refractor: optimized imports

--- src/Dashboard/Careerhub/Interests.js
+++ src/Dashboard/Careerhub/Interests.js
@@ -1,12 +1,14 @@
 import {useNavigate} from "react-router-dom";
+import {useMediaQuery} from "react-responsive";
 
 const Interests = () => {
+    const isMediumScreen = useMediaQuery({minWidth: 1370});
     const navigate = useNavigate()
     const handleNavigation = () => {
         navigate('/careerhub/profile/career')
     }
     return (
-        <div className="flex md:flex-row flex-col items-center h-[200px] bg-[#f7f7f7] mt-10">
+        <div className={`flex md:flex-row flex-col ${!isMediumScreen ? "mx-12 p-4 relative" : "flex px-4 pt-3.5 mx-4"} items-center w-[94%] border h-[200px] mt-10`}>
             <div className="flex flex-col p-6">
                 <h1 className="mb-4 font-medium md:text-xl">Take 15 mins to set your career interests</h1>
                 <p className="md:w-[80%] text-sm md:text-lg">Tell us what you want to achieve in your career, and we'll
