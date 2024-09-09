be720f916c27a0e4c73ac881a344229a59d72134
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-09-08 20:55:51 -0500

    feat: parent component that displays both date picker component and feedback requests

--- src/Dashboard/Feedback/Feedback.js
+++ src/Dashboard/Feedback/Feedback.js
@@ -1,40 +1,36 @@
-import NavBar from "../Careerhub/NavBar";
 import 'react-datepicker/dist/react-datepicker.css'
 import DateRange from "./DateRange";
-import Positions from "./Positions";
 import FeedbackRequests from "./FeedbackRequests";
 import {useMediaQuery} from "react-responsive";
+import {useState} from "react";
+import {addMonths} from "date-fns";
+import GenericRibbon from "../Careerhub/GenericRibbon";
 
 const Feedback = () => {
-    const isMediumScreen = useMediaQuery({minWidth: 900}); // Set the breakpoint for md screens
+    const isMediumScreen = useMediaQuery({minWidth: 1040}); // Set the breakpoint for md screens
+    const [selectedStartDate, setSelectedStartDate] = useState(addMonths(new Date(), -1))
+    const [selectedEndDate, setSelectedEndDate] = useState(new Date())
 
     return (
 
-        <div className="ml-3">
-            <NavBar/>
-            <div>
+        <div className="bg-[#f0f1f2] h-screen">
+            <GenericRibbon text="My Feedbacks"/>
+            <div className="h-screen">
                 <div
-                    className={`flex ${isMediumScreen ? "gap-x-6" : "flex-col gap-y-6"} justify-center items-center mt-6`}>
+                    className={`flex ${isMediumScreen ? "gap-x-6" : "flex-col gap-y-6"} justify-center items-center`}>
                     {isMediumScreen ? (
-                        <>
-                            <div className="flex flex-col justify-center items-center ml-20">
-                                <div>
-                                    <h1 className="text-3xl font-semibold ml-2 mb-6">Feedback Center</h1>
-                                    <DateRange/>
-                                    <Positions/>
-                                </div>
-                            </div>
-                            <div className="border mt-36 p-3 mr-24">
-                                <FeedbackRequests/>
-                            </div>
-                        </>
+                        <div className="flex space-x-10 w-full mx-6 py-10">
+                            <DateRange setSelectedStartDate={setSelectedStartDate}
+                                       setSelectedEndDate={setSelectedEndDate}/>
+                            <FeedbackRequests startDate={selectedStartDate} endDate={selectedEndDate}/>
+
+                        </div>
                     ) : (
                         <>
-                            <div className={"flex flex-col"}>
-                                <h1 className="text-3xl font-semibold ml-2">Feedback Center</h1>
-                                <DateRange/>
-                                <Positions/>
-                                <FeedbackRequests/>
+                            <div className="flex flex-col w-full">
+                                <DateRange setSelectedStartDate={setSelectedStartDate}
+                                           setSelectedEndDate={setSelectedEndDate}/>
+                                <FeedbackRequests startDate={selectedStartDate} endDate={selectedEndDate}/>
                             </div>
                         </>
                     )}
