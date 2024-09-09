9c954d4a4030b61925b5a3e060fa8da438e7e8e5
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-09-08 20:54:36 -0500

    feat: added new functions that updates states when user selects dates

--- src/Dashboard/Feedback/DateRange.js
+++ src/Dashboard/Feedback/DateRange.js
@@ -5,8 +5,11 @@ import DatePicker from "react-datepicker";
 import {useEffect, useRef, useState} from "react";
 import {addMonths, format} from "date-fns";
 import 'react-datepicker/dist/react-datepicker.css'
+import {useMediaQuery} from "react-responsive";
 
-const DateRange = () => {
+const DateRange = ({ setSelectedStartDate, setSelectedEndDate}) => {
+    const isMediumScreen = useMediaQuery({minWidth: 1040}); // Set the breakpoint for md screens
+
     const [calendarShow, setCalendarShow] = useState(false)
     const [startDate, setStartDate] = useState(addMonths(new Date(), -1))
     const [endDate, setEndDate] = useState(new Date())
@@ -42,11 +45,22 @@ const DateRange = () => {
         else
             setCalendarShow(!calendarShow)
     }
+    const handleStartDateChange = (date)=>{
+        setStartDate(date)
+        setSelectedStartDate(date)
+    }
+
+    const handleEndDateChange = (date)=>{
+        setEndDate(date)
+        setSelectedEndDate(date)
+        setCalendarShow(false)
+    }
     return (
-        <div>
-            <div className="flex justify-center items-center cursor-pointer"
+        <div className={`${isMediumScreen ? "w-[50%]" : "my-4 mx-6"} border border-gray-300 rounded-xl p-4 h-[48vh] bg-white`}>
+            <div className="flex cursor-pointer"
                  onClick={() => setDateRangeShow(!dateRangeShow)}>
-                <h1 className={`flex font-medium ${dateRangeShow ? "mb-2" : "mb-1"} ml-3 mt-3`}>Data Range</h1>
+                <h1 className={`flex font-medium ${dateRangeShow ? "mb-2" : "mb-1"} ml-3 mt-3 w-full`}>Data
+                    Range</h1>
                 {
                     !dateRangeShow ?
                         <div className="ml-auto md:ml-40 mr-4">
@@ -62,15 +76,15 @@ const DateRange = () => {
             {
                 dateRangeShow && (
                     <div>
-                        <div className="flex text-gray border w-[280px] p-1.5 ">
+                        <div className="flex text-gray border border-gray-300 justify-center items-center rounded-md w-[95%] p-1.5 ">
                             <input placeholder="Start date" value={getFormattedDate(startDate)}
-                                   className="w-[100px] h-[24px] outline-none focus:border-b-2 focus:border-[#367c2b] pb-1 cursor-pointer"
-                                   onClick={handleCalendarShow} onChange={(e)=> setStartDate(e.target.value)}/>
-                            <MdArrowRightAlt size={24} color="gray" className="ml-auto mr-2"/>
+                                   className="w-full h-[24px] outline-none focus:border-b-2 focus:border-[#367c2b] pb-1 cursor-pointer"
+                                   onClick={handleCalendarShow} onChange={(e) => setStartDate(e.target.value)}/>
+                            <MdArrowRightAlt size={30} color="gray" className="ml-4 mr-2"/>
                             <input placeholder="End date" value={getFormattedDate(endDate)}
-                                   className="w-[100px] h-[24px] outline-none focus:border-b-2 focus:border-[#367c2b] pb-1 ml-2 cursor-pointer"
-                                   onClick={handleCalendarShow} onChange={(e)=> setStartDate(e.target.value)}/>
-                            <BsCalendar4 size={14} color="gray" className="ml-auto mr-1 mt-1 cursor-pointer"/>
+                                   className="w-full h-[24px] outline-none focus:border-b-2 focus:border-[#367c2b] pb-1 ml-2 cursor-pointer"
+                                   onClick={handleCalendarShow} onChange={(e) => setStartDate(e.target.value)}/>
+                            <BsCalendar4 size={24} color="gray" className="ml-4 mt-1 cursor-pointer"/>
                         </div>
                     </div>
                 )
@@ -79,34 +93,34 @@ const DateRange = () => {
                 calendarShow && (
                     <div className="absolute flex mt-1" ref={ref}>
                         <div>
-                            <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} inline/>
+                            <DatePicker selected={startDate} onChange={(date)=>handleStartDateChange(date)} inline/>
 
                         </div>
                         <div>
-                            <DatePicker selected={endDate} onChange={(date) => setEndDate(date)} inline/>
+                            <DatePicker selected={endDate} onChange={(date) => handleEndDateChange(date)} inline/>
 
                         </div>
                     </div>
                 )
             }
             <div
-                className={`${calendarShow ? "mt-4" : "mt-3"}flex justify-center items-center mt-2 text-lg mr-4 text-[#4f5666] w-full md:[w-350px] pb-2`}>
+                className={`${calendarShow ? "mt-4" : "mt-3"} mt-2 text-lg mr-4 text-[#4f5666] w-full pb-2`}>
                 <div onClick={() => handleLinksClick("pending")}
-                     className={`${navLinksShow.pending ? "flex justify-center items-center p-2 bg-[#feffe6] cursor-pointer text-[#20571a] md:w-[280px] mr-4" :
-                         "flex p-2 hover:bg-[#feffe6] cursor-pointer hover:text-[#20571a] md:w-[280px] mr-4"}`}>
+                     className={`${navLinksShow.pending ? "flex justify-center items-center p-2 bg-[#feffe6] cursor-pointer text-[#20571a] mr-4" :
+                         "flex p-2 hover:bg-[#feffe6] cursor-pointer hover:text-[#20571a] mr-4"}`}>
                     <h1>Pending Feedback</h1>
                     <p className="ml-auto">0</p>
                 </div>
                 <div onClick={() => handleLinksClick("completed")}
-                     className={`${navLinksShow.completed ? "flex p-2 bg-[#feffe6] cursor-pointer text-[#20571a] md:w-[280px] mr-4" :
-                         "flex p-2 hover:bg-[#feffe6] cursor-pointer hover:text-[#20571a] md:w-[280px] mr-4"}`}>
+                     className={`${navLinksShow.completed ? "flex p-2 bg-[#feffe6] cursor-pointer text-[#20571a] mr-4" :
+                         "flex p-2 hover:bg-[#feffe6] cursor-pointer hover:text-[#20571a] mr-4"}`}>
                     <h1 className={`${navLinksShow.completed ? "text-[#20571a]" :
                         "hover:text-[#20571a]"}`}>Completed Feedback</h1>
                     <p className="ml-auto">0</p>
                 </div>
                 <div onClick={() => handleLinksClick("archived")}
-                     className={`${navLinksShow.archived ? "flex p-2 bg-[#feffe6] cursor-pointer text-[#20571a] md:w-[280px] mr-4" :
-                         "flex p-2 hover:bg-[#feffe6] cursor-pointer hover:text-[#20571a] md:w-[280px] mr-4"}`}>
+                     className={`${navLinksShow.archived ? "flex p-2 bg-[#feffe6] cursor-pointer text-[#20571a] mr-4" :
+                         "flex p-2 hover:bg-[#feffe6] cursor-pointer hover:text-[#20571a] mr-4"}`}>
                     <h1>Archived Feedback</h1>
                     <p className="ml-auto">0</p>
                 </div>
