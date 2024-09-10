8494f2ab0603b7904bcd87c66ad3b8591a22836a
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-09-10 17:25:49 -0500

    feat: added a new state variable that tracks if uploadedJobs object has been populated with the response from the backend

--- src/Dashboard/Careerhub/Management/ManagementHub.js
+++ src/Dashboard/Careerhub/Management/ManagementHub.js
@@ -7,16 +7,19 @@ import GenericRibbon from "../GenericRibbon";
 import CompanyInfo from "./CompanyInfo";
 import {useMediaQuery} from "react-responsive";
 import NoAvailableJobs from "../Jobs/AvailableJobs/NoAvailableJobs";
+import {ScaleLoader} from "react-spinners";
 
 const ManagementHub = () => {
     const [uploadedJobs, setUploadedJobs] = useState([])
     const [isInitialized, setIsInitialized] = useState(false)
+    const [loading, setLoading] = useState(false)
     const userInfo = JSON.parse(localStorage.getItem('user'))
     const employerEmail = userInfo.email
     const role = userInfo.role
     const mediaQuery = useMediaQuery({minWidth: "1400px"})
 
     useEffect(() => {
+        setLoading(true)
         const fetchJobsUploaded = async () => {
             try {
                 const response = await getUploadedJobs(employerEmail, new AbortController())
@@ -27,7 +30,9 @@ const ManagementHub = () => {
                 setIsInitialized(true)
             } catch (err) {
                 console.error(err.message + err)
+            } finally {
                 setIsInitialized(true)
+                setLoading(false)
             }
         }
         fetchJobsUploaded().catch(err => console.error(err))
@@ -35,6 +40,12 @@ const ManagementHub = () => {
 
     return (
         <div>
+            {loading && (
+                <div className="fixed flex justify-center items-center inset-0 backdrop-brightness-50">
+                    <ScaleLoader color="#1c3e17" height={100} width={4}/>
+                </div>
+            )}
+
             {isInitialized && (
                 <>
                     <ToastContainer position="top-center"/>
